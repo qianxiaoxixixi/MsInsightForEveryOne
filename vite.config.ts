045de616029605c98217f31ae363e788c247756c
insightDeveloper <insightDeveloper>
2023-08-30 15:37:34 +0800

    update
    Match-id-ea3a85c8b036bdd7b9572f411977d609480dbf3a

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
       '@plugins': fileURLToPath(new URL('./plugins', import.meta.url)),
       '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
       '@router': fileURLToPath(new URL('./src/router/index.ts', import.meta.url)),
-      '@centralServer': fileURLToPath(new URL('./src/centralServer/websocket/messageManager.ts', import.meta.url)),
+      '@centralServer': fileURLToPath(new URL('./src/centralServer/server.ts', import.meta.url))
       '@assets': fileURLToPath(new URL('./src/assets', import.meta.url))
     }
   }
