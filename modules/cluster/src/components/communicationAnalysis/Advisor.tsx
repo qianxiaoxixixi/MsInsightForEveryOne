c1de21452dc88e3a6e91fdc5dcb0f4206dd72ee7
insightDeveloper <insightDeveloper>
2023-09-19 14:25:20 +0800

    [********][feat]前端适配FrameWork框架
    Match-id-fec05e3647ec95cea7543f7e5f176c57713e5571

---
+++ modules/cluster/src/components/communicationAnalysis/Advisor.tsx
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2023-2023. All rights reserved.
+ */
+import React from 'react';
+
+const Advisor = (): JSX.Element => {
+    return (
+        <div></div>
+    );
+};
+
+export default Advisor;
---
+++ modules/cluster/src/pages/LoadingPage.tsx
@@ -0,0 +1,53 @@
+import styled from '@emotion/styled/dist/emotion-styled.cjs';
+import { observer } from 'mobx-react';
+import React from 'react';
+
+const ImgWithFallback = ({
+    className = '',
+}): JSX.Element => {
+    const PictureContainer = styled.picture`
+      img {
+        height: 48px;
+        width: 48px;
+      }
+    `;
+    return (
+        <PictureContainer>
+            <div className={className}></div>
+        </PictureContainer>
+    );
+};
+
+const StatePopover = observer(() => {
+    const Mask = styled.div`
+      position: absolute;
+      display: flex;
+      z-index: 4;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      width: 100%;
+      height: 100%;
+      justify-content: center;
+      align-items: center;
+      background-color: ${props => props.theme.maskColor};
+    `;
+    const Info = styled.div`
+      border-radius: 4px;
+      position: relative;
+      display: flex;
+      align-items: center;
+      width: 16rem;
+      height: 3rem;
+      font-size: 1.12rem;
+      .img {
+        margin-right: 10px;
+      }
+    `;
+    return <Mask>
+        <Info className={'info'}><ImgWithFallback className={'loading'}/>waiting...</Info>
+    </Mask>;
+});
+
+export default StatePopover;
