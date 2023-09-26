0a537c135b177ccafa3a97eef456739c88f22526
insightDeveloper <1390732074@qq.com>
2023-09-26 15:26:00 +0800

    修改Memory模块打包方式
    # message auto-generated for no-merge-commit merge:
    merge br_feature_NewFrameworkImpl_****** into br_feature_NewFrameworkImpl_******
    
    修改Memory模块打包方式
    
    Created-by: insightDeveloper
    
    Match-id-ae2b07799efb117b915a70612dde34e5cf1b56b5

--- modules/memory/craco.config.js
+++ modules/memory/craco.config.js
@@ -1,28 +1,14 @@
 /*
  * Copyright (c) Huawei Technologies Co., Ltd. 2023-2023. All rights reserved.
  */
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-
-const htmllist = [
-  new HtmlWebpackPlugin({
-    template: 'public/index.html',
-    filename: 'memory.html',
-    chunks: ['memory'],
-  }),
-];
 
 module.exports = {
-    webpack: {
-        configure: (webpackConfig) => {
-            // Because CEF has issues with loading source maps properly atm,
-            // lets use the best we can get in line with `inline-source-map`
-            webpackConfig.entry = {
-                main: webpackConfig.entry,
-                index: './src/index.tsx',
-            };
-            webpackConfig.output.filename = 'static/js/[name].bundle.js';
-            webpackConfig.devtool = 'inline-source-map';
-            return webpackConfig;
-        },
+  webpack: {
+    configure: (webpackConfig) => {
+      // Because CEF has issues with loading source maps properly atm,
+      // lets use the best we can get in line with `inline-source-map`
+      webpackConfig.devtool = 'inline-source-map';
+      return webpackConfig;
     },
+  },
 };
