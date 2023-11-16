e399d7bb17521ac85cb9c8c2d66a0fc18b8e99df
insightDeveloper <insightDeveloper>
2023-11-16 19:58:04 +0800

    多线程处理请求
    (cherry picked from commit ******d29624faafe7833fbc******ef7a1)
    
    Match-id-2b76e0fb32d6df0f796c185b301d331cc470066f

--- server/src/modules/timeline/core/database/TraceDatabase.h
+++ server/src/modules/timeline/core/database/TraceDatabase.h
@@ -23,7 +23,7 @@ namespace Module {
 namespace Timeline {
 class TraceDatabase : public Database {
 public:
-    TraceDatabase() = default;
+    explicit TraceDatabase(std::mutex &sqlMutex);
     ~TraceDatabase() override;
 
     bool SetConfig();
@@ -76,6 +76,7 @@ public:
     bool QueryStepDuration(const std::string& stepId, uint64_t &min, uint64_t &max);
 
 private:
+    std::mutex &mutex;
     const std::string sliceTable = "slice";
     const std::string threadTable = "thread";
     const std::string processTable = "process";
