f05a60f9a7c5d5efb568564468f4626d6b36b5d3
insightDeveloper <insightDeveloper>
2023-12-12 15:19:09 +0800

    C++ UT覆盖率统计脚本
    # message auto-generated for no-merge-commit merge:
    merge master_1212 into master
    
    C++ UT覆盖率统计脚本
    
    Created-by: insightDeveloper
    
    Match-id-11225e3805af65641be0366454a0ed03d3d60c37

---
+++ build/cpp_coverage.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# This script is used to generate llt-cpp coverage.
+# Copyright Huawei Technologies Co., Ltd. 2023-2023. All rights reserved.
+
+set -e
+CUR_DIR=$(dirname $(readlink -f $0))
+TOP_DIR=${CUR_DIR}/..
+cd ${TOP_DIR}/server/build
+python3 download_third_party.py
+python3 preprocess_third_party.py
+python3 build.py test
+
+#
+COV_DIR=${TOP_DIR}/build_llt/output/cpp_coverage
+BUILD_DIR=${TOP_DIR}/server/build
+
+if [ ! -d ${COV_DIR} ] ; then
+    mkdir -p ${COV_DIR}
+fi
+
+cd ../output/linux-x86_64/bin
+
+./insight_test
+
+cd ${BUILD_DIR}
+echo ${BUILD_DIR}
+test_dir=`find ${BUILD_DIR} -name "insight_test.dir"`
+echo ${test_dir}
+echo "********************** Generate insight_test Coverage Start.************************"
+lcov -c -d ${test_dir} -o ${COV_DIR}/lcov_insight_test.info --rc lcov_branch_coverage=1
+lcov -r ${COV_DIR}/lcov_insight_test.info '*c++*' -o ${COV_DIR}/lcov_insight_test.info --rc lcov_branch_coverage=1
+lcov -r ${COV_DIR}/lcov_insight_test.info '*test*' -o ${COV_DIR}/lcov_insight_test.info --rc lcov_branch_coverage=1
+lcov -r ${COV_DIR}/lcov_insight_test.info '*third_party*' -o ${COV_DIR}/lcov_insight_test.info --rc lcov_branch_coverage=1
+echo "********************** Generate insight_test Coverage Stop.*************************"
+
+genhtml ${COV_DIR}/lcov_insight_test.info -o ${COV_DIR}/result --branch-coverage
+echo "report: ${COV_DIR}"
