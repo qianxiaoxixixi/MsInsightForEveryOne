3f3c69e5a6f5e75fd0dc61a0026bf571058341ca
insightDeveloper <1390732074@qq.com>
2023-12-21 11:49:50 +0800

    补充工具类的UT测试代码
    # message auto-generated for no-merge-commit merge:
    merge server_ut into master
    
    补充工具类的UT测试代码
    
    Created-by: insightDeveloper
    
    Match-id-9fe4e5ed864ccbed6a30ac83c06e1eef1f67ba0d

---
+++ server/src/test/utils/RegexUtilTest.cpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2023-2023. All rights reserved.
+ */
+
+#include <gtest/gtest.h>
+#include "../TestSuit.cpp"
+#include "RegexUtil.h"
+
+using namespace Dic;
+
+
+TEST(RegexUtil, RegexSearch) {
+    std::string data = "AaaAaaaA";
+    std::string pattern;
+    std::optional<std::smatch> smatch;
+
+    pattern = "a+";
+    smatch = RegexUtil::RegexSearch(data, pattern);
+    EXPECT_EQ(smatch->str(), "aa");
+
+    pattern = "aaAa+";
+    smatch = RegexUtil::RegexSearch(data, pattern);
+    EXPECT_EQ(smatch->str(), "aaAaaa");
+
+    pattern = "AaaAaaaA";
+    smatch = RegexUtil::RegexSearch(data, pattern);
+    EXPECT_EQ(smatch->str(), "AaaAaaaA");
+
+    pattern = "ww";
+    smatch = RegexUtil::RegexSearch(data, pattern);
+    EXPECT_EQ(smatch.has_value(), false);
+}
+
+TEST(RegexUtil, RegexMatch) {
+    std::string data = "AaaAaaaA";
+    std::string pattern;
+    std::optional<std::smatch> smatch;
+
+    pattern = "AaaAaaaA";
+    smatch = RegexUtil::RegexMatch(data, pattern);
+    EXPECT_EQ(smatch.value()[0], "AaaAaaaA");
+
+    pattern = "Aa+Aa+aA";
+    smatch = RegexUtil::RegexMatch(data, pattern);
+    EXPECT_EQ(smatch.value()[0], "AaaAaaaA");
+
+    pattern = "\\w+";
+    smatch = RegexUtil::RegexMatch(data, pattern);
+    EXPECT_EQ(smatch.value()[0], "AaaAaaaA");
+
+    pattern = "[a-zA-z]+";
+    smatch = RegexUtil::RegexMatch(data, pattern);
+    EXPECT_EQ(smatch.value()[0], "AaaAaaaA");
+}
