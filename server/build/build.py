bc36654687cfbcbe44c17deb7615ad057429644d
insightDeveloper <insightDeveloper>
2023-12-25 09:54:04 +0800

    安全编译选项
    Match-id-5fc6f3bd143984ea0ab2cf64632e008fa8dd4670

--- server/build/build.py
+++ server/build/build.py
@@ -125,6 +125,8 @@ def build_bin(args):
         'pyinstaller', '--collect-submodules', 'cluster_analyse', '--path=' + CLUSTER_ANALYSE_DIR,
         '--distpath=' + att_bin_dir, '--specpath', CMAKE_BUILD_DIR, '--onefile', att_main_path
     ]
+    if not IS_WINDOWS:
+        build_att.append('-s')
     output = subprocess.Popen(build_att, cwd=BUILD_DIR, stdout=subprocess.PIPE)
     log_output(output)
 
