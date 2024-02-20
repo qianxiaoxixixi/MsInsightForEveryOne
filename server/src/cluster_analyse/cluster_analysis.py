091a40def483ca08e9d3ac5b3eabea87775d715d
insightDeveloper <insightDeveloper>
2024-02-20 19:01:07 +0800

    同步att代码
    # message auto-generated for no-merge-commit merge:
    merge master into master
    
    同步att代码
    
    Created-by: insightDeveloper
    
    Match-id-58fb290b43256a4fa6ef4b7e62f6a732e7b7ffac

--- server/src/cluster_analyse/cluster_analysis.py
+++ server/src/cluster_analyse/cluster_analysis.py
@@ -16,12 +16,14 @@
 # limitations under the License.
 
 import argparse
+import os
 import multiprocessing
 import sys
 from multiprocessing import freeze_support
 
 from cluster_data_preprocess.pytorch_data_preprocessor import PytorchDataPreprocessor
 from communication_group.communication_group_generator import CommunicationGroupGenerator
+from cluster_data_preprocess.mindspore_data_preprocessor import MindsporeDataPreprocessor
 from common_func.constant import Constant
 from common_func.file_manager import FileManager
 from common_func.path_manager import PathManager
@@ -29,28 +31,49 @@ from analysis.analysis_facade import AnalysisFacade
 
 
 class Interface:
-    def __init__(self, args: argparse.Namespace):
-        self.collection_path = PathManager.get_realpath(args.collection_path)
+    ASCEND_PT = "ascend_pt"
+    ASCEND_MS = "ascend_ms"
+
+    def __init__(self, params: dict):
+        self.collection_path = PathManager.get_realpath(params.get(Constant.COLLECTION_PATH))
+        self.analysis_mode = params.get(Constant.ANALYSIS_MODE)
         self.data_map = {}
         self.communication_group = {}
         self.collective_group_dict = {}
         self.communication_ops = []
         self.matrix_ops = []
 
+    def allocate_prof_data(self):
+        ascend_pt_dirs = []
+        ascend_ms_dirs = []
+        for root, dirs, _ in os.walk(self.collection_path):
+            for dir_name in dirs:
+                if dir_name.endswith(self.ASCEND_PT):
+                    ascend_pt_dirs.append(os.path.join(root, dir_name))
+                if dir_name.endswith(self.ASCEND_MS):
+                    ascend_ms_dirs.append(os.path.join(root, dir_name))
+        pt_data_map = PytorchDataPreprocessor(ascend_pt_dirs).get_data_map()
+        ms_data_map = MindsporeDataPreprocessor(ascend_ms_dirs).get_data_map()
+        if pt_data_map and ms_data_map:
+            print("[ERROR] Can not analyze pytorch and mindspore meantime.")
+            return[]
+        return pt_data_map if pt_data_map else ms_data_map
+
     def run(self):
         PathManager.check_input_directory_path(self.collection_path)
         PathManager.check_path_owner_consistent(self.collection_path)
         FileManager.create_output_dir(self.collection_path)
-        data_map = PytorchDataPreprocessor(self.collection_path).get_data_map()
+        data_map = self.allocate_prof_data()
         if not data_map:
             print("[WARNING] Can not get rank info or profiling data.")
             return
-        comm_data_dict = CommunicationGroupGenerator(self.collection_path, data_map).generate()
         params = {
             Constant.COLLECTION_PATH: self.collection_path,
             Constant.DATA_MAP: data_map,
-            Constant.COMM_DATA_DICT: comm_data_dict
+            Constant.ANALYSIS_MODE: self.analysis_mode
         }
+        comm_data_dict = CommunicationGroupGenerator(params).generate()
+        params[Constant.COMM_DATA_DICT] = comm_data_dict
         AnalysisFacade(params).cluster_analyze()
 
 
@@ -60,5 +83,11 @@ if __name__ == "__main__":
     freeze_support()
     parser = argparse.ArgumentParser(description="cluster analysis module")
     parser.add_argument('-d', '--collection_path', type=str, required=True, help="profiling data path")
+    parser.add_argument('-m', '--mode', choices=['all', 'communication_time', 'communication_matrix'],
+                        default='all', help="different analysis mode")
     args_parsed = parser.parse_args()
-    Interface(args_parsed).run()
+    parameter = {
+        Constant.COLLECTION_PATH: args_parsed.collection_path,
+        Constant.ANALYSIS_MODE: args_parsed.mode
+    }
+    Interface(parameter).run()
