609895f704da298a7357e6f19679fe952fe4fe46
insightDeveloper <1390732074@qq.com>
2024-02-22 15:12:10 +0800

    [********]fix(cleancode) 修复cleancode
    # message auto-generated for no-merge-commit merge:
    merge ********_master into master
    
    [********]fix(cleancode) 修复cleancode
    
    Created-by: insightDeveloper
    
    Match-id-a7dd458b0abc2cee9579f39b6066d731199456e0

--- server/src/utils/FileUtil.cpp
+++ server/src/utils/FileUtil.cpp
@@ -4,26 +4,28 @@
 #include "FileUtil.h"
 
 namespace Dic {
-    std::string FileUtil::GetCurrPath()
-    {
-        char currPath[1024];
-        std::string strCurrPath;
+std::string FileUtil::GetCurrPath()
+{
+    char currPath[1024];
+    std::string strCurrPath;
 #ifdef _WIN32
-        ::GetModuleFileName(NULL, currPath, MAX_PATH);
-        (_tcsrchr(currPath, '\\'))[1] = 0;
-        strCurrPath = StringUtil::GbkToUtf8(currPath);
-#elif __APPLE__
-        uint32_t size = sizeof(currPath);
-        if (_NSGetExecutablePath(currPath, &size) == 0) {
-            strCurrPath = std::string(dirname(currPath));
-        }
+    ::GetModuleFileName(NULL, currPath, MAX_PATH);
+    (_tcsrchr(currPath, '\\'))[1] = 0;
+    strCurrPath = StringUtil::GbkToUtf8(currPath);
 #else
-        ssize_t len = readlink("/proc/self/exe", currPath, sizeof(currPath) - 1);
-        if (len != -1) {
-            currPath[len] = '\0';
-            strCurrPath = std::string(dirname(currPath));
-        }
-#endif
-        return strCurrPath;
+#ifdef __APPLE__
+    uint32_t size = sizeof(currPath);
+    if (_NSGetExecutablePath(currPath, &size) == 0) {
+        strCurrPath = std::string(dirname(currPath));
     }
+#else
+    ssize_t len = readlink("/proc/self/exe", currPath, sizeof(currPath) - 1);
+    if (len != -1) {
+        currPath[len] = '\0';
+        strCurrPath = std::string(dirname(currPath));
+    }
+#endif
+#endif
+    return strCurrPath;
+}
 }
