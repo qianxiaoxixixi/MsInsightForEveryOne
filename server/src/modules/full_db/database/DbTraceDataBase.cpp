1b62693ea8920f794d329c077baa603448a21c98
insightDeveloper <1390732074@qq.com>
2024-03-26 17:33:46 +0800

    适配文件名称修改
    # message auto-generated for no-merge-commit merge:
    merge table_name_change into master
    
    适配表格名称修改
    
    Created-by: insightDeveloper
    
    Match-id-679fc21e135c129f5d308499e56bc9f841fc65a7

--- server/src/modules/full_db/database/DbTraceDataBase.cpp
+++ server/src/modules/full_db/database/DbTraceDataBase.cpp
@@ -733,7 +733,11 @@ std::vector<std::string> DbTraceDataBase::QueryRankId()
     if (type == FileType::MS_PROF) {
         sql = "SELECT id FROM " + TABLE_NPU_INFO;
     } else if (type == FileType::PYTORCH) {
-        sql = "SELECT rankId FROM " + TABLE_PYTORCH_INFO;
+        if (CheckTableDataInvalid(TABLE_PYTORCH_INFO)) {
+            sql = "SELECT DISTINCT rank_id FROM " + TABLE_PYTORCH_INFO;
+        } else {
+            sql = "SELECT DISTINCT id FROM " + TABLE_NPU_INFO;
+        }
     }
     std::vector<std::string> rankIds;
     int result = sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, nullptr);
