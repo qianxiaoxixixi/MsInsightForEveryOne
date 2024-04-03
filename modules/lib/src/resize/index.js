3399ac71f906edca09d83a700afaf4ca38817cfe
insightDeveloper <1390732074@qq.com>
2024-04-03 11:22:03 +0800

    提取前端公共组件
    Match-id-96e21c4e59ec146bc6879cf1cdc301ec1cc9db1a

---
+++ modules/lib/src/resize/index.js
@@ -0,0 +1,7 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
+ */
+import ResizeTable from './ResizeTable';
+import Resizor from './Resizor';
+
+export default { ResizeTable, Resizor };
