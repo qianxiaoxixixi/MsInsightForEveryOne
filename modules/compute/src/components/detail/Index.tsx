d8cd69637f323c3efccba1ea528318a226fee37a
insightDeveloper <insightDeveloper>
2024-04-28 19:56:19 +0800

    [********][feat]支持算子计算负载展示
    Match-id-4cf2517c8031a1af8cb6abe89cc642a2fd7248d8

--- modules/compute/src/components/detail/Index.tsx
+++ modules/compute/src/components/detail/Index.tsx
@@ -2,15 +2,20 @@
  * Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
  */
 import React from 'react';
-import BaseInfo from './BaseInfo';
+import { observer } from 'mobx-react';
 import { type Session } from '../../entity/session';
+import BaseInfo from './BaseInfo';
+import ComputeWorkload from './ComputeWorkload/Index';
 
-function Index({ session }: { session: Session }): JSX.Element {
+const index = observer(({ session }: { session: Session }): JSX.Element => {
     return (
-        <div style={{ padding: '0 20px' }}>
-            <BaseInfo session={session}/>
+        <div style={{ padding: '0 20px', height: '100%', overflow: 'auto' }}>
+            <div>
+                <BaseInfo session={session}/>
+                <ComputeWorkload session={session}/>
+            </div>
         </div>
     );
-}
+});
 
-export default Index;
+export default index;
