9179d56d7ebc0fdfe4022804c84e35d0fa5cbd4d
insightDeveloper <1390732074@qq.com>
2024-05-22 19:50:49 +0800

    专家建议-增加融合算子识别的实现
    # message auto-generated for no-merge-commit merge:
    merge 1123 into master
    
    专家建议-增加融合算子识别的实现
    
    Created-by: insightDeveloper
    
    Match-id-991d960eaf4ffd4874aea54437c5fa5134f69157

--- server/src/modules/advisor/process/FusedOpAdvisor.h
+++ server/src/modules/advisor/process/FusedOpAdvisor.h
@@ -5,10 +5,29 @@
 #ifndef PROFILER_SERVER_FUSEDOPADVISOR_H
 #define PROFILER_SERVER_FUSEDOPADVISOR_H
 
+#include "VirtualTraceDatabase.h"
 #include "AdvisorProtocolRequest.h"
 #include "AdvisorProtocolResponse.h"
 
 namespace Dic::Module::Advisor {
+const std::vector<Timeline::FuseableOpRule> FUSEABLE_OPERATER_RULE_LIST = {
+    {{"Gelu", "Add"}, "GeluAdd", ""},
+    {{"Add", "LayerNorm"}, "AddLayerNorm", ""},
+    {{"Add", "LayerNormV3"}, "AddLayerNorm", ""},
+    {{"Cast", "LayerNorm", "Cast"}, "LayerNorm", ""},
+    {{"Add", "DropOutDoMask", "Add"}, "bias_dropout_add", ""},
+    {{"MatMulV2", "Swish", "MatMulV2", "Mul", "MatMulV2"}, "FFN", ""},
+    {{"Transpose", "Transpose", "GatherElement", "Transpose"}, "GatherElement", ""},
+    {{"Mul", "Slice", "Neg", "Slice", "ConcatD", "Mul", "Add"}, "RotaryMul", ""},
+    {{"Mul", "AsStrided", "Neg", "AsStrided", "ConcatD", "Mul", "Add"}, "RotaryMul", ""},
+    {{"Mul", "Slice", "Neg", "Slice", "ConcatD", "Cast", "Mul", "Add"}, "RotaryMul", ""},
+    {{"Cast", "Square", "MemSet", "ReduceMean", "Add", "Rsqrt", "Mul", "Cast", "Mul"}, "RMSNorm", ""},
+    {
+        {"Cast", "Square", "ReduceMeanD", "Add", "Rsqrt", "Cast", "Cast", "Mul", "Cast", "Cast", "Mul", "Cast"},
+        "RMSNORM",
+        ""
+    },
+};
 
 class FusedOpAdvisor {
 public:
