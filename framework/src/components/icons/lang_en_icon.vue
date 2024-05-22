39c3f8090c7e39fa437f3f9933150f6769b4b06c
insightDeveloper <1390732074@qq.com>
2024-05-22 16:28:32 +0800

    [********]<feature>支持中英文切换(1)-framework翻译包实例化
    # message auto-generated for no-merge-commit merge:
    merge 0506-i18n-slice-1 into master
    
    支持中英文切换(1)-framework翻译包实例化
    
    Created-by: insightDeveloper
    
    Match-id-b69a9f06ab9a38cbed6fb7389cbc977f963f5563

---
+++ framework/src/components/icons/lang_en_icon.vue
@@ -0,0 +1,46 @@
+<template>
+    <!-- Generator: Adobe Illustrator 23.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+    <svg
+        version="1.1"
+        id="图层_1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        x="0px"
+        y="0px"
+        viewBox="0 0 16 16"
+        style="enable-background: new 0 0 16 16"
+        xml:space="preserve"
+        class="icon"
+        fill="currentColor"
+    >
+        <g>
+            <g id="编组-2" transform="translate(2.000000, 2.000000)">
+                <path
+                    id="路径"
+                    class="st0"
+                    d="M12,5.5C12,5.8,11.8,6,11.5,6c-0.2,0-0.4-0.2-0.5-0.4l0-0.1V1H1v10h4.5c0.2,0,0.4,0.2,0.5,0.4
+			l0,0.1c0,0.2-0.2,0.4-0.4,0.5l-0.1,0H0.9c-0.5,0-0.9-0.4-0.9-0.8l0-0.1V0.9C0,0.5,0.4,0.1,0.8,0l0.1,0h10.1c0.5,0,0.9,0.4,0.9,0.8
+			l0,0.1V5.5z"
+                />
+                <path
+                    id="形状结合"
+                    class="st1"
+                    d="M5.2,7L4.3,9.4C4.2,9.6,3.9,9.7,3.7,9.7C3.4,9.6,3.3,9.3,3.4,9l0,0l2.4-6.6
+			c0.1-0.1,0.2-0.2,0.3-0.3c0.2-0.1,0.5,0,0.6,0.2l0,0.1L8.4,7H5.2z M6.2,4L5.5,6H7L6.2,4z"
+                />
+                <g id="编组" transform="translate(7.000000, 6.500000)">
+                    <path
+                        class="st0"
+                        d="M5,1.5c0.6,0,1,0.4,1,1v1c0,0.6-0.4,1-1,1H1c-0.6,0-1-0.4-1-1v-1c0-0.6,0.4-1,1-1H5z M5,2.5H1v1h4V2.5z"
+                    />
+                    <path
+                        id="矩形备份-5"
+                        class="st1"
+                        d="M3,0C2.7,0,2.5,0.2,2.5,0.5c0,0,0,0,0,0l0,5C2.5,5.8,2.7,6,3,6c0.3,0,0.5-0.2,0.5-0.5
+				c0,0,0,0,0,0l0-5C3.5,0.2,3.3,0,3,0z"
+                    />
+                </g>
+            </g>
+        </g>
+    </svg>
+</template>
---
+++ framework/src/utils/local-storage.ts
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
+ */
+export enum LocalStorageKeys {
+    LANGUAGE = 'language',
+}
+
+class LocalStorageService {
+    getItem(key: LocalStorageKeys): any {
+        const item = localStorage.getItem(key);
+        return item ? JSON.parse(item) : null;
+    }
+
+    setItem(key: LocalStorageKeys, value: any): void {
+        localStorage.setItem(key, JSON.stringify(value));
+    }
+
+    removeItem(key: LocalStorageKeys): void {
+        localStorage.removeItem(key);
+    }
+
+    clear(): void {
+        localStorage.clear();
+    }
+}
+
+export const localStorageService = new LocalStorageService();
+export default localStorageService;
