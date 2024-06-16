bdcf52bd80463b321d8753e3199276eb0ce20cf2
insightDeveloper <insightDeveloper>
2024-06-16 21:22:20 +0800

    修复多级多卡导致summary界面数据无法查询的问题
    Match-id-fc5a07ff9eb9646bb5c60bb5f3268c89d6016967

--- modules/timeline/src/insight/units/utils.ts
+++ modules/timeline/src/insight/units/utils.ts
@@ -1,15 +1,24 @@
 import type { Theme } from '@emotion/react';
 import type { Session } from '../../entity/session';
 import type { CardMetaData } from '../../entity/data';
+import type { InsightUnit } from '../../entity/insight';
 
 export const colorPalette: Array<keyof Theme['colorPalette']> = ['slateblue', 'royalblue', 'skyblue', 'turquoise', 'olivedrab', 'yellowgreen',
     'gold', 'orange', 'coral', 'orangered', 'palevioletred', 'mediumorchid'];
 
 export function getTimeOffset(session: Session, cardId?: string): number {
-    const unit = session.units.find(value => (value.metadata as CardMetaData).cardId === cardId);
+    const unit = session.units.find(value => containCardId(value, cardId ?? ''));
     const realCardId = unit ? (unit.metadata as CardMetaData).cardId : 'Host';
     // 查询泳道chart参数加上时间偏移
     return cardId !== undefined
         ? (session?.unitsConfig.offsetConfig.timestampOffset as Record<string, number>)?.[realCardId] ?? 0
         : 0;
+}
+
+// 判断cardUnit自身以及子单元是否包含指定的cardId
+function containCardId(unit: InsightUnit, cardId: string): boolean {
+    if ((unit.metadata as CardMetaData)?.cardId === cardId) {
+        return true;
+    }
+    return unit.children?.some(childUnit => containCardId(childUnit, cardId)) ?? false;
 }
