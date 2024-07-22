df8993d3fff15feb5bbee887c21a2377c60f3bc7
insightDeveloper <1390732074@qq.com>
2024-07-22 21:10:33 +0800

    [********][fix]修改热点函数指令表表头筛选页面卡顿问题
    Match-id-e09f633531bb9930a5afdd2e00b9058cffa4600a

--- modules/lib/src/resize/ResizeTable.tsx
+++ modules/lib/src/resize/ResizeTable.tsx
@@ -1,11 +1,14 @@
 /*
  * Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
 */
-import React, { cloneElement, useState, useEffect, useRef, useCallback, useMemo } from 'react';
+import React, { cloneElement, useState, useEffect, useRef, useMemo } from 'react';
 import { Table } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
+import { isArray } from 'lodash';
 import Resizor from './Resizor';
+import { getColumnSearchProps } from './ColumnFilterWithSelection';
 import { limitInput } from '../utils/Common';
+import { useWatchVirtualRender } from '../utils/VirtualRenderUtils';
 interface ExtendsColumnType {minWidth?: number};
 
 interface IResizableTitleProps extends React.ReactElement {
@@ -57,87 +60,21 @@ const getResizeColumns = ({ columns, index, width, nextWidth, minThWidth, variab
 };
 
 // ============================ virtual ============================
-const getTableSize = (prop:
-{
-    scroll?: {x?: number;y?: number;rowHeight?: number};
-    dataSource: unknown[];
-},
-): { [prop: string]: number} => {
-    const { scroll, dataSource } = prop;
-    const visibleHeight = scroll?.y ?? 0;
-    const itemHeight = scroll?.rowHeight ?? 39;
-    const visibleCount = Math.ceil(visibleHeight / itemHeight) + 2;
-    const totalCount = dataSource.length;
-    const totalHeight = itemHeight * totalCount;
-    return { visibleHeight, itemHeight, visibleCount, totalCount, totalHeight };
-};
-const handleScrollEvent = (e: any, { virtual, itemHeight, visibleCount, tableRef, setRange }:
-{ virtual: boolean;
-    itemHeight: number;
-    visibleCount: number;
-    tableRef: React.MutableRefObject<HTMLElement | undefined>;
-    setRange: React.Dispatch<React.SetStateAction<[number, number]>>;
-},
-): void => {
-    if (!virtual || itemHeight === 0) {
-        return;
-    }
-    const startIdx = Math.floor(e.target.scrollTop / itemHeight);
-    const endIdx = startIdx + visibleCount;
-    setRange([startIdx, endIdx]);
-    const offset = startIdx * itemHeight;
-    if (tableRef?.current !== undefined) {
-        (tableRef.current as any).style.top = `${offset}px`;
-    }
-};
-const addVirtualEvent = ({ virtual, divRef, tableRef, scrollEvent, height }:
-{
-    virtual: boolean;
-    divRef: React.MutableRefObject<null>;
-    tableRef: React.MutableRefObject<HTMLElement | undefined>;
-    scrollEvent: (e: any) => void;
-    height: number;
-},
-) : (() => void) => {
-    const voidFun = (): void => {};
-    if (!virtual || divRef.current === null) {
-        return voidFun;
-    }
-    const parentNode = (divRef.current as HTMLElement).querySelector('.ant-table-body') as HTMLElement;
-    const table = (divRef.current as HTMLElement).querySelector('.ant-table-body table') as HTMLElement;
-    if (table === null || parentNode === null) {
-        return voidFun;
-    }
-    tableRef.current = table;
-    // 占位div
-    const placeholderWrapper = document.createElement('div');
-    placeholderWrapper.style.height = `${height}px`;
-    parentNode.appendChild(placeholderWrapper);
-    parentNode.style.position = 'relative';
-    if (height === 0) {
-        Object.assign(table.style, { position: 'relative' });
-    } else {
-        Object.assign(table.style, { position: 'absolute', top: '0', left: '0' });
-    }
-    // 添加滚动事件
-    parentNode.addEventListener('scroll', scrollEvent);
-    parentNode.scrollTo({ top: 0 });
-    return (): void => {
-        // 清理占位div
-        parentNode.removeChild(placeholderWrapper);
-        parentNode.removeEventListener('scroll', scrollEvent);
-    };
+const getVirtualElement = (dom: Element): [Element | null, Element | null] => {
+    const box = dom.querySelector('.ant-table-body');
+    const target = dom.querySelector('.ant-table-body table');
+    return [box, target];
 };
 
 function ResizeTable<T extends object>(prop: Iprop<T>): JSX.Element {
     const { columns: propColumns, variableTotalWidth = false, minThWidth = 50, id, style, virtual = false, scroll, dataSource, ...restProps } = prop;
-    const [columns, setColumns] = useState<ColumnsType<T>>(propColumns ?? []);
+    const [columns, setColumns] = useState<ColumnsType<T>>([]);
 
     // ============================ Resize ============================
     useEffect(() => {
         setColumns(propColumns ?? []);
     }, [JSON.stringify(propColumns)]);
-    const mergeColumns: any = columns.map((col, index) => ({
+    const mergeColumns: any = useMemo(() => columns.map((col, index) => ({
         ...col,
         onHeaderCell: () => ({
             onResize: (deltaX: number, width: number, nextWidth?: number): void => {
@@ -146,21 +83,24 @@ function ResizeTable<T extends object>(prop: Iprop<T>): JSX.Element {
             },
             resizable: variableTotalWidth || index !== propColumns.length - 1,
         }),
-    }));
+        // ============================ filters ============================
+        ...((isArray(col.filters) && col.filters.length > 0) ? getColumnSearchProps() : {}),
+    })), [columns]);
 
     // ============================ virtual ============================
-    const myRef = useRef(null);
-    const tableRef = useRef<HTMLElement>();
-    const [range, setRange] = useState<[number, number]>([0, 0]);
-    const { itemHeight, visibleCount, totalHeight } = useMemo(() => getTableSize(prop), [dataSource, scroll]);
-    const scrollEvent = useCallback((e: any) => {
-        handleScrollEvent(e, { virtual, itemHeight, visibleCount, tableRef, setRange });
-    }, [visibleCount, totalHeight, virtual]);
+    const ref = useRef(null);
+    const { data: renderList, boxRef, targetRef } = useWatchVirtualRender(
+        { visibleHeight: scroll?.y ?? 0, itemHeight: scroll?.rowHeight ?? 39, dataSource });
     useEffect(() => {
-        setRange([0, visibleCount]);
-        return addVirtualEvent({ virtual, divRef: myRef, tableRef, scrollEvent, height: totalHeight });
-    }, [scrollEvent, totalHeight, virtual]);
-    const renderList = useMemo(() => dataSource.slice(...range), [dataSource, range]);
+        if (!virtual || ref.current === null) {
+            return;
+        }
+        const [box, target] = getVirtualElement(ref.current as Element);
+        if (box !== null && target !== null) {
+            (boxRef as any).current = box;
+            (targetRef as any).current = target;
+        }
+    }, []);
 
     // 出现分页跳转输入框
     useEffect(() => {
@@ -170,18 +110,14 @@ function ResizeTable<T extends object>(prop: Iprop<T>): JSX.Element {
     }, [prop.pagination]);
 
     return (
-        <div id={id} style={{ ...style ?? {} }} ref={myRef}>
+        <div id={id} style={{ ...style ?? {} }} ref={ref}>
             <Table
                 { ...restProps }
                 scroll={scroll}
                 dataSource={virtual ? renderList : dataSource}
                 className={!variableTotalWidth ? '' : 'variableTotalWidth'}
                 columns={mergeColumns}
-                components={{
-                    header: {
-                        cell: resizableTitle,
-                    },
-                }}
+                components={{ header: { cell: resizableTitle } }}
             />
         </div>
     );
