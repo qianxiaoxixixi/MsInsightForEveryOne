7c3112b51b780afee70de18e3d200ed9c308a09e
insightDeveloper <insightDeveloper>
2024-08-08 15:27:47 +0800

    add operator ut
    # message auto-generated for no-merge-commit merge:
    merge DT into master
    
    add operator ut
    
    Created-by: insightDeveloper
    
    Match-id-ff787f7f0bd7d882ec738abb4cd4b1240ba05dac

---
+++ server/src/test/modules/operator/handler/OperatorRequestHandlerTest.cpp
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
+ */
+
+#include <gtest/gtest.h>
+#include <WsSessionManager.h>
+#include "OperatorRequestHandler.h"
+#include "QueryOpCategoryInfoHandler.h"
+#include "QueryOpComputeUnitHandler.h"
+#include "QueryOpStatisticInfoHandler.h"
+#include "QueryOpDetailInfoHandler.h"
+#include "QueryOpMoreInfoHandler.h"
+
+class OperatorRequestHandlerTest : public ::testing::Test {
+public:
+    static void SetUpTestCase()
+    {
+        Dic::Server::WsChannel *ws;
+        std::unique_ptr<Dic::Server::WsSession> session = std::make_unique<Dic::Server::WsSession>(ws);
+        Dic::Server::WsSessionManager::Instance().AddSession(std::move(session));
+    }
+    static void TearDownTestCase()
+    {
+        Dic::Server::WsSessionManager::Instance().GetSession()->Stop();
+        Dic::Server::WsSessionManager::Instance().RemoveSession();
+    }
+};
+
+TEST_F(OperatorRequestHandlerTest, QueryOpCategoryInfoHandlerNormalTest)
+{
+    Dic::Module::Operator::QueryOpCategoryInfoHandler handler;
+    auto requestPtr = std::make_unique<Dic::Protocol::OperatorCategoryInfoRequest>();
+    requestPtr.get()->params.rankId = "0";
+    requestPtr.get()->params.group = "Operator";
+    requestPtr.get()->params.topK = -1;
+    ASSERT_NO_THROW(handler.HandleRequest(std::move(requestPtr)));
+}
+
+TEST_F(OperatorRequestHandlerTest, QueryOpComputeUnitHandlerNormalTest)
+{
+    Dic::Module::Operator::QueryOpComputeUnitHandler handler;
+    auto requestPtr = std::make_unique<Dic::Protocol::OperatorComputeUnitInfoRequest>();
+    requestPtr.get()->params.rankId = "0";
+    ASSERT_NO_THROW(handler.HandleRequest(std::move(requestPtr)));
+}
+
+TEST_F(OperatorRequestHandlerTest, QueryOpStatisticInfoHandlerNormalTest)
+{
+    Dic::Module::Operator::QueryOpStatisticInfoHandler handler;
+    auto requestPtr = std::make_unique<Dic::Protocol::OperatorStatisticInfoRequest>();
+    ASSERT_NO_THROW(handler.HandleRequest(std::move(requestPtr)));
+}
+
+TEST_F(OperatorRequestHandlerTest, QueryOpDetailInfoHandlerNormalTest)
+{
+    Dic::Module::Operator::QueryOpDetailInfoHandler handler;
+    auto requestPtr = std::make_unique<Dic::Protocol::OperatorDetailInfoRequest>();
+    ASSERT_NO_THROW(handler.HandleRequest(std::move(requestPtr)));
+}
+
+TEST_F(OperatorRequestHandlerTest, QueryOpMoreInfoHandlerNormalTest)
+{
+    Dic::Module::Operator::QueryOpMoreInfoHandler handler;
+    auto requestPtr = std::make_unique<Dic::Protocol::OperatorMoreInfoRequest>();
+    ASSERT_NO_THROW(handler.HandleRequest(std::move(requestPtr)));
+}
