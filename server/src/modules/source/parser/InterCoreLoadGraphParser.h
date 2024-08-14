abc6172477c296308b06cb2856c8a255c69d5b6f
insightDeveloper <1390732074@qq.com>
2024-08-14 12:24:17 +0800

    [********]<feature>核间负载数据解析功能头文件定义
    Match-id-042acad280fee9aaa60683f981445ee7bbda1b9d

---
+++ server/src/modules/source/parser/InterCoreLoadGraphParser.h
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
+ */
+
+#ifndef PROFILER_SERVER_INTERCORELOADGRAPHPARSER_H
+#define PROFILER_SERVER_INTERCORELOADGRAPHPARSER_H
+
+#include <string>
+#include <vector>
+#include "SourceProtocolResponse.h"
+
+namespace Dic {
+namespace Module {
+namespace Source {
+
+struct InterCoreSubCoreDetail {
+    std::string subCoreType;
+    uint8_t subCoreIndex = 0;
+    uint64_t cycles = 0;
+    float throughput = 0;
+    float hitRate = 0;
+};
+
+struct InterCoreOpDetail {
+    uint8_t coreId = 0;
+    std::vector<InterCoreSubCoreDetail> subCoreDetails = {};
+};
+
+struct InterCoreLoadAnalysisDetail {
+    std::string soc;
+    std::string opType;
+    std::string advice;
+    std::vector<InterCoreOpDetail> opDetails = {};
+    uint64_t minCycle = 0;
+    float minThroughput = 0;
+    float maxHitRate = 0;
+
+    void AddOpDetail(InterCoreOpDetail&& opDetail)
+    {
+        opDetails.emplace_back(opDetail);
+    }
+
+    void SetMinCycle(uint64_t cycle)
+    {
+        if (cycle <= 0) {
+            return;
+        }
+        if (minCycle == 0) {
+            minCycle = cycle;
+            return;
+        }
+        minCycle = (cycle < minCycle ? cycle : minCycle);
+    }
+
+    void SetMinThroughput(float throughput)
+    {
+        if (throughput <= 0) {
+            return;
+        }
+        if (minThroughput == 0) {
+            minThroughput = throughput;
+            return;
+        }
+        minThroughput = (throughput < minThroughput ? throughput : minThroughput);
+    }
+
+    void SetMaxHitRate(float hitRate)
+    {
+        maxHitRate = (hitRate > maxHitRate ? hitRate : maxHitRate);
+    }
+};
+
+class InterCoreLoadGraphParser {
+public:
+    bool GetInterCoreLoadAnalysisInfo(std::string& json, Dic::Protocol::DetailsInterCoreLoadGraphBody& body);
+
+private:
+    InterCoreLoadAnalysisDetail ParseInterCoreLoadAnalysisInfo(std::string& json);
+    void CalculateColorLevel();
+};
+
+} // Dic
+} // Module
+} // Source
+
+#endif // PROFILER_SERVER_INTERCORELOADGRAPHPARSER_H
