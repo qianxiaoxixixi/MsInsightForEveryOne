143ddd574cca75ed2cb861cb0e66a25c10af8714
insightDeveloper <1390732074@qq.com>
2024-10-30 14:42:49 +0800

    修复Task表和MSTX_EVENT的异常数据导致页面卡住的问题
    # message auto-generated for no-merge-commit merge:
    merge fix_wrong_data_master into master
    
    修复Task表和MSTX_EVENT的异常数据导致页面卡住的问题
    
    Created-by: insightDeveloper
    
    Match-id-d63ba241d2ba6e013f2df4041ea3a943da329f0e

--- server/src/defs/ConstantDefs.h
+++ server/src/defs/ConstantDefs.h
@@ -29,6 +29,9 @@ static const std::string ATT_MODEL_DEFAULT = "all";
 static const std::string ATT_MODEL_TIME = "communication_time";
 static const std::string ATT_MODEL_MATRIX = "communication_matrix";
 
+// db wrong data
+static const std::string WRONG_DATA = std::to_string(UINT32_MAX);
+
 } // end of namespace Dic
 
 #endif // DATA_INSIGHT_CORE_CONSTANTS_DEFS_H
