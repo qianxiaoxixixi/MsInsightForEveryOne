07705760a9d36ea83b3d9070da32ac868e7a23ec
insightDeveloper <insightDeveloper>
2024-11-12 19:19:23 +0800

    优化mstx和cann泳道点击算子详情逻辑
    Match-id-5c16b315d0ca7755cc9102bf60aa155ff85a3fff

--- server/src/modules/timeline/core/dataEngine/CannApiRepo.h
+++ server/src/modules/timeline/core/dataEngine/CannApiRepo.h
@@ -5,6 +5,8 @@
 #ifndef PROFILER_SERVER_CANNAPIREPO_H
 #define PROFILER_SERVER_CANNAPIREPO_H
 #include "CannApiTable.h"
+#include "StringIdsTable.h"
+#include "EnumApiTypeTable.h"
 #include "SliceRepoInterface.h"
 namespace Dic::Module::Timeline {
 class CannApiRepo : public SliceRepoInterface {
@@ -19,10 +21,13 @@ public:
         std::unordered_map<uint64_t, std::pair<std::string, std::string>> &threadInfo) override;
     void QueryCompeteSliceByIds(const SliceQuery &sliceQuery, const std::vector<uint64_t> &sliceIds,
         std::vector<CompeteSliceDomain> &CompeteSliceVec) override;
+    bool QuerySliceDetailInfo(const SliceQuery &sliceQuery, CompeteSliceDomain &competeSliceDomain);
     void SetCannApiTable(std::unique_ptr<CannApiTable>);
 
-private:
+protected:
+    std::unique_ptr<EnumApiTypeTable> apiTypeTable = std::make_unique<EnumApiTypeTable>();
     std::unique_ptr<CannApiTable> cannApiTable = std::make_unique<CannApiTable>();
+    std::unique_ptr<StringIdsTable> stringIdsTable = std::make_unique<StringIdsTable>();
 };
 }
 #endif // PROFILER_SERVER_CANNAPIREPO_H
