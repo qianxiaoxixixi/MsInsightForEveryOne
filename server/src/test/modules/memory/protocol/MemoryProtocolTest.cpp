3c92143d224485673bf384c76bc5e6ad93aa89c1
insightDeveloper <1390732074@qq.com>
2024-11-12 20:48:30 +0800

    增加memory模块protocol的ut
    # message auto-generated for no-merge-commit merge:
    merge build into master
    
    增加memory模块protocol的ut
    
    Created-by: insightDeveloper
    
    Match-id-24f94d4d225795f0a51c3bbe3d74624e32f02c24

--- server/src/test/modules/memory/protocol/MemoryProtocolTest.cpp
+++ server/src/test/modules/memory/protocol/MemoryProtocolTest.cpp
@@ -1,3 +1,51 @@
 /*
  * Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
  */
+
+#include <gtest/gtest.h>
+#include "MemoryProtocolUtil.h"
+#include "MemoryProtocolRequest.h"
+#include "MemoryProtocol.h"
+
+using namespace Dic::Protocol;
+class MemoryProtocolTest : public ::testing::Test {
+protected:
+    void SetUp() override
+    {
+        memoryProtocol.Register();
+    }
+
+    void TearDown() override
+    {
+        memoryProtocol.UnRegister();
+    }
+    Dic::Protocol::MemoryProtocol memoryProtocol;
+};
+
+TEST_F(MemoryProtocolTest, ToMemoryViewRequestTestReturnNormal)
+{
+    std::string reqJson = R"({"id": 2, "moduleName": "memory", "type": "request", "command": "Memory/view/memoryUsage",
+        "resultCallbackId": 0, "params": {"rankId": "1", "type": "Overall", "isCompare": false}})";
+    MemoryComponentParams expect = {"1", "Overall", false};
+    Dic::document_t json;
+    json.Parse(reqJson.c_str());
+    std::string err;
+    auto result = dynamic_cast<MemoryViewRequest &>(*(memoryProtocol.FromJson(json, err))).params;
+    EXPECT_EQ(result.rankId, expect.rankId);
+    EXPECT_EQ(result.type, expect.type);
+    EXPECT_EQ(result.isCompare, expect.isCompare);
+}
+
+TEST_F(MemoryProtocolTest, ToMemoryOperatorSizeRequestTestReturnNormal)
+{
+    std::string reqJson = R"({"id": 2, "moduleName": "memory", "type": "request", "resultCallbackId": 0,
+        "command": "Memory/view/operator/size", "params": {"rankId": "1", "type": "Overall"}})";
+    MemoryComponentParams expect = {"1", "Overall", false};
+    Dic::document_t json;
+    json.Parse(reqJson.c_str());
+    std::string err;
+    auto result = dynamic_cast<MemoryOperatorSizeRequest &>(*(memoryProtocol.FromJson(json, err))).params;
+    EXPECT_EQ(result.rankId, expect.rankId);
+    EXPECT_EQ(result.type, expect.type);
+    EXPECT_EQ(result.isCompare, expect.isCompare);
+}
