a11cc7959cbcc63a95e55eabc31e9d1b4049f5dc
insightDeveloper <1390732074@qq.com>
2024-12-02 15:35:26 +0800

    fix: mac上复制粘贴等快捷键不生效
    # message auto-generated for no-merge-commit merge:
    merge feat/mac-menu into master
    
    【fix】【现网问题】【美团】修复mac版本mindstudio insight详细信息数据不能支持拷贝的问题
    
    Created-by: insightDeveloper
    
    Match-id-de75ae4f17382dff3ff7f5719b751a946bf78d4b

--- platform/src/main.rs
+++ platform/src/main.rs
@@ -4,47 +4,46 @@
 
 #![windows_subsystem = "windows"]
 
+#[cfg(windows)]
+use std::os::windows::process::CommandExt;
 use std::{
     env,
     env::current_exe,
-    fs::{create_dir_all, read},
-    io,
-    io::{BufRead, BufReader},
+    fs::read,
     path::PathBuf,
-    process::{Child, Command, Stdio},
-    sync::Mutex,
+    process::{Child, Command},
+    sync::{Arc, Mutex},
 };
-#[cfg(any(target_os = "macos", target_os = "linux"))]
-use std::collections::{HashMap, VecDeque};
+
 #[cfg(windows)]
-use std::os::windows::process::CommandExt;
+use webview2err::show_webview_err_message;
+#[cfg(feature = "default")]
+#[cfg(target_os = "macos")]
+use wry::application::menu::{MenuBar, MenuItem};
 #[cfg(feature = "default")]
 use wry::{
     application::{
         event::{Event, WindowEvent},
-        event_loop::{ControlFlow, EventLoop},
+        event_loop::{ControlFlow, EventLoop, EventLoopProxy},
         window::{Window, WindowBuilder},
     },
     http::{header::CONTENT_TYPE, Response},
-    webview::{webview_version, WebViewBuilder, FileDropEvent},
+    webview::{webview_version, WebView, FileDropEvent, WebViewBuilder},
 };
-#[cfg(windows)]
-use webview2err::show_webview_err_message;
 
+use crate::cleanup::handle_close_requested;
+
+mod cleanup;
 #[cfg(windows)]
 mod webview2err;
 
 const SERVER_RELATIVE_LIST: [&str; 4] =
     ["resources", "profiler", "server", "profiler_server"];
+#[cfg(windows)]
 const NO_WINDOW_FLAG: u32 = 0x08000000;
 const MIMETYPE_HTML: &str = "text/html";
 
-fn run_server(
-    root_path: &PathBuf,
-    cache_path: &PathBuf,
-    port: &mut String,
-    sid: &String,
-) -> Option<Mutex<Child>> {
+fn server_path(root_path: &PathBuf) -> Option<PathBuf> {
     let mut server_path = root_path.to_path_buf();
 
     #[cfg(target_os = "macos")]
@@ -59,11 +58,12 @@ fn run_server(
 
     #[cfg(target_os = "linux")]
     {
-        let ld_library_path = env::var_os("LD_LIBRARY_PATH").unwrap_or_default();
+        let ld_library_path =
+            env::var_os("LD_LIBRARY_PATH").unwrap_or_default();
         let new_ld_library_path = format!(
             "{}:{}",
-             server_path.join("resources/profiler/server/").display(),
-             ld_library_path.to_string_lossy()
+            server_path.join("resources/profiler/server/").display(),
+            ld_library_path.to_string_lossy()
         );
         env::set_var("LD_LIBRARY_PATH", &new_ld_library_path);
     }
@@ -73,16 +73,23 @@ fn run_server(
     }
 
     #[cfg(windows)]
-    {
-        server_path.set_extension("exe");
-    }
+    server_path.set_extension("exe");
 
     if !server_path.exists() {
         return None;
     }
-    let Some(path) = server_path.to_str() else {
-        return None;
-    };
+
+    Some(server_path)
+}
+
+fn run_server(
+    root_path: &PathBuf,
+    cache_path: &PathBuf,
+    port: &mut String,
+    sid: &String,
+) -> Option<Mutex<Child>> {
+    let binding = server_path(root_path)?;
+    let Some(path) = binding.to_str() else { return None };
 
     let output = Command::new(path)
         .arg("--scan=9000")
@@ -101,9 +108,7 @@ fn run_server(
     let mut server_command = Command::new(path);
 
     #[cfg(windows)]
-    {
-        server_command.creation_flags(NO_WINDOW_FLAG);
-    }
+    server_command.creation_flags(NO_WINDOW_FLAG);
 
     match server_command
         .arg(format!("--wsPort={}", port))
@@ -116,41 +121,73 @@ fn run_server(
     }
 }
 
-// run script
-#[cfg(feature = "default")]
-fn run_script(
-    server_process: Mutex<Child>,
-    root_path: &PathBuf,
-    port: &str,
-    sid: &String,
-) -> wry::Result<()> {
-    let event_loop = EventLoop::with_user_event();
-    
-    let proxy = event_loop.create_proxy();
+#[cfg(target_os = "macos")]
+fn macos_menu() -> MenuBar {
+    let mut menu = MenuBar::new();
 
-    let resource_path = root_path.to_path_buf();
-    let window: Window = WindowBuilder::new()
-        .with_title("MindStudio Insight")
-        .with_maximized(true)
-        .build(&event_loop)
-        .expect("Error occurred when create App window");
+    let mut window_menu = MenuBar::new();
+    window_menu.add_native_item(MenuItem::Minimize);
+    window_menu.add_native_item(MenuItem::Hide);
+    window_menu.add_native_item(MenuItem::HideOthers);
+    window_menu.add_native_item(MenuItem::Separator);
+    window_menu.add_native_item(MenuItem::Services);
+    window_menu.add_native_item(MenuItem::Separator);
+    window_menu.add_native_item(MenuItem::CloseWindow);
+    window_menu.add_native_item(MenuItem::Quit);
 
-    #[cfg(windows)]
-    {
-        use wry::application::window::Icon;
-        use wry::application::platform::windows::IconExtWindows;
+    menu.add_submenu("Window", true, window_menu);
 
-        window.set_window_icon(
-            Icon::from_path(
-                resource_path.join("resources/images/icons/mindstudio.ico"),
-                None,
-            )
-            .ok(),
-        );
+    let mut edit_menu = MenuBar::new();
+    edit_menu.add_native_item(MenuItem::Cut);
+    edit_menu.add_native_item(MenuItem::Copy);
+    edit_menu.add_native_item(MenuItem::Paste);
+    edit_menu.add_native_item(MenuItem::SelectAll);
+
+    menu.add_submenu("Edit", true, edit_menu);
+
+    menu
+}
+
+#[cfg(windows)]
+fn set_windows_icon(window: &Window, root_path: &PathBuf) {
+    use wry::application::{platform::windows::IconExtWindows, window::Icon};
+
+    window.set_window_icon(
+        Icon::from_path(
+            root_path
+                .to_path_buf()
+                .join("resources/images/icons/mindstudio.ico"),
+            None,
+        )
+        .ok(),
+    );
+}
+
+fn extract_mimetype(path: &str) -> &str {
+    let mut mimetype = MIMETYPE_HTML;
+    if let Some((_, ext)) = path.rsplit_once('.') {
+        mimetype = match ext {
+            "html" => "text/html",
+            "js" => "text/javascript",
+            "css" => "text/css",
+            "svg" => "image/svg+xml",
+            "png" => "image/png",
+            "wasm" => "application/wasm",
+            _ => MIMETYPE_HTML,
+        }
     }
 
-    let webview = WebViewBuilder::new(window)
-        .expect("Failed to create webview builder")
+    mimetype
+}
+
+fn create_webview(
+    window: Window,
+    resource_path: Arc<PathBuf>,
+    port: &str,
+    sid: &String,
+    proxy: Arc<EventLoopProxy<PathBuf>>,
+) -> wry::Result<WebView> {
+    WebViewBuilder::new(window)?
         .with_custom_protocol("wry".into(), move |request| {
             let path = request.uri().path();
             let content = match read(resource_path.join(&path[1..]).as_path()) {
@@ -158,30 +195,19 @@ fn run_script(
                 Err(e) => return Err(wry::Error::Io(e)),
             };
 
-            let mut mimetype = MIMETYPE_HTML;
-            if let Some((_, ext)) = path.rsplit_once('.') {
-                mimetype = match ext {
-                    "html" => "text/html",
-                    "js" => "text/javascript",
-                    "css" => "text/css",
-                    "svg" => "image/svg+xml",
-                    "png" => "image/png",
-                    "wasm" => "application/wasm",
-                    _ => MIMETYPE_HTML,
-                }
-            }
+            let mimetype = extract_mimetype(path);
 
-            Response::builder().header(CONTENT_TYPE, mimetype).body(content).map_err(Into::into)
+            Response::builder()
+                .header(CONTENT_TYPE, mimetype)
+                .body(content)
+                .map_err(Into::into)
         })
-        .with_url(
-            format!("wry://localhost/resources/profiler/frontend/index.html?port={}&sid={}", port, sid)
-                .as_str(),
-        )?
-        .with_file_drop_handler(move |window, ev| {
+        .with_url(format!("wry://localhost/resources/profiler/frontend/index.html?port={}&sid={}", port, sid).as_str())?
+        .with_file_drop_handler(move |_, ev| {
             match ev {
                 FileDropEvent::Dropped(paths) => {
                     if let Err(e) = proxy.send_event(paths[0].to_owned()) {
-                        eprintln!("app closed unexpectly: {:#?}", e);
+                        eprintln!("app closed unexpectedly: {:#?}", e);
                     }
                 }
                 _ => {}
@@ -190,148 +216,69 @@ fn run_script(
             true
         })
         .build()
-        .expect("Failed to create webview");
+}
 
+fn handle_user_event(webview: &WebView, path: PathBuf) {
+    if let Err(e) =
+        webview.evaluate_script(&format!("window.handleDrop({:#?})", path))
+    {
+        eprintln!("drop-file ipc failed: {:#?}", e);
+    }
+}
+
+fn run_event_loop(
+    event_loop: EventLoop<PathBuf>,
+    server_process: Arc<Mutex<Child>>,
+    webview: WebView,
+) {
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
         match event {
             Event::WindowEvent {
                 event: WindowEvent::CloseRequested, ..
             } => {
-                *control_flow = {
-                    let mut server_process_guard = server_process
-                        .lock()
-                        .expect("Failed to lock server-process mutex");
-                    let pid = server_process_guard.id();
-                    match query_child_pids(pid) {
-                        Ok(child_pids) => {
-                            for child_pid in child_pids {
-                                if let Err(e) = kill_process_tree(child_pid) {
-                                    eprintln!("Err when kill process: {e}")
-                                }
-                            }
-                        }
-                        Err(e) => eprintln!("Err when query child pids {e}"),
-                    }
-                    server_process_guard
-                        .kill()
-                        .expect("server process could not be killed");
-                    ControlFlow::Exit
-                }
+                *control_flow = handle_close_requested(server_process.clone());
             }
-            Event::UserEvent(path) => {
-                if let Err(e) = webview.evaluate_script(&format!("window.handleDrop({:#?})",path)) {
-                    eprintln!("drop-file ipc failed: {:#?}", e);
-                }
-            }
+            Event::UserEvent(path) => handle_user_event(&webview, path),
             _ => (),
         }
     });
 }
 
-fn query_child_pids(parent_pid: u32) -> io::Result<Vec<String>> {
+// run script
+#[cfg(feature = "default")]
+fn run_script(
+    server_process: Arc<Mutex<Child>>,
+    root_path: &PathBuf,
+    port: &str,
+    sid: &String,
+) -> wry::Result<()> {
+    let event_loop = EventLoop::with_user_event();
+
+    let proxy = Arc::new(event_loop.create_proxy());
+
+    let mut window_builder: WindowBuilder = WindowBuilder::new()
+        .with_title("MindStudio Insight")
+        .with_maximized(true);
+
+    #[cfg(target_os = "macos")]
+    {
+        let menu = macos_menu();
+        window_builder = window_builder.with_menu(menu);
+    }
+
+    let window = window_builder
+        .build(&event_loop)
+        .expect("Error occurred when create App window");
+
+    let resource_path = Arc::new(root_path.to_path_buf());
+
     #[cfg(windows)]
-    {
-        let wmic_output = Command::new("wmic")
-            .arg("process")
-            .arg("where")
-            .arg(format!("ParentProcessId={}", parent_pid))
-            .arg("get")
-            .arg("ProcessId")
-            .creation_flags(0x08000000)
-            .stdout(Stdio::piped())
-            .spawn();
+    set_windows_icon(&window, root_path);
 
-        let mut child_pids = Vec::new();
-        match wmic_output {
-            Ok(child) => {
-                if let Some(stdout) = child.stdout {
-                    BufReader::new(stdout)
-                        .lines()
-                        .skip(1)
-                        .filter_map(|line| line.ok())
-                        .map(|line| line.trim().to_string())
-                        .filter(|s| !s.is_empty())
-                        .for_each(|pid| child_pids.push(pid));
-                }
-            }
-            Err(e) => eprintln!("wmic command failed: {e}"),
-        }
-        Ok(child_pids)
-    }
+    let webview = create_webview(window, resource_path, port, sid, proxy)?;
 
-    #[cfg(any(target_os = "macos", target_os = "linux"))]
-    {
-        let ps_output = Command::new("ps")
-            .arg("-o")
-            .arg("ppid,pid")
-            .stdout(Stdio::piped())
-            .spawn();
-        let mut child_pids = Vec::new();
-        let mut pid_map: HashMap<String, Vec<String>> = HashMap::new();
-        match ps_output {
-            Ok(pairs) => {
-                if let Some(stdout) = pairs.stdout {
-                    let reader = BufReader::new(stdout);
-                    for line in reader.lines().skip(1) {
-                        if let Ok(line) = line {
-                            let parts: Vec<&str> =
-                                line.split_whitespace().collect();
-                            if let [key, value] = parts.as_slice() {
-                                pid_map
-                                    .entry(key.to_string())
-                                    .and_modify(|v| v.push(value.to_string()))
-                                    .or_insert(vec![value.to_string()]);
-                            }
-                        }
-                    }
-                }
-            }
-            Err(e) => eprintln!("ps command failed: {e}"),
-        }
-
-        let mut deque = VecDeque::new();
-        deque.push_back(parent_pid.to_string());
-        while let Some(cur) = deque.pop_back() {
-            if let Some(children) = pid_map.get(&cur) {
-                for item in children {
-                    child_pids.push(item.to_string());
-                    deque.push_back(item.to_string());
-                }
-            }
-        }
-
-        Ok(child_pids)
-    }
-}
-
-fn kill_process_tree(parent_pid: String) -> io::Result<()> {
-    #[cfg(windows)]
-    {
-        // Windows 下使用 taskkill 命令
-        Command::new("taskkill")
-            .arg("/f")
-            .arg("/t") // 终止包括子进程在内的所有进程
-            .arg("/im")
-            .arg(parent_pid.to_string())
-            .creation_flags(0x08000000)
-            .stdout(Stdio::null())
-            .stderr(Stdio::null())
-            .output()
-            .expect("Failed to execute taskkill command");
-    }
-    #[cfg(any(target_os = "macos", target_os = "linux"))]
-    {
-        Command::new("kill")
-            .arg("-9")
-            .arg(parent_pid.to_string())
-            .stdout(Stdio::null())
-            .stderr(Stdio::null())
-            .status()
-            .expect("Failed to execute kill command");
-    }
-
-    Ok(())
+    Ok(run_event_loop(event_loop, server_process, webview))
 }
 
 fn home_dir() -> Option<PathBuf> {
@@ -393,8 +340,12 @@ fn main() {
 
     if !cache_path.exists() {
         #[cfg(windows)]
-        create_dir_all(cache_path.as_path())
-            .expect("no permission to create cache_path");
+        {
+            use std::fs::create_dir_all;
+
+            create_dir_all(cache_path.as_path())
+                .expect("no permission to create cache_path");
+        }
 
         #[cfg(unix)]
         {
@@ -410,28 +361,32 @@ fn main() {
 
     if webview_version().is_err() {
         #[cfg(windows)]
-        {
-            show_webview_err_message();
-        }
+        show_webview_err_message();
+
         return;
     }
 
     let mut port: String = String::new();
     let sid = uuid::Uuid::new_v4().to_string();
-    // start the server
+
     if let Some(child) = run_server(&root_path, &cache_path, &mut port, &sid) {
-        let _ = run_script(child, &root_path, &port.as_str(), &sid);
+        let _ = run_script(Arc::new(child), &root_path, &port.as_str(), &sid);
     }
 }
-use std::process::{exit, ExitStatus};
+
 #[cfg(not(feature = "default"))]
 fn main() {
+    use std::process::{exit, ExitStatus};
+
     let args: Vec<String> = env::args().collect();
     let profiler_path = current_exe()
         .expect("Failed to get current exe path")
         .parent()
         .expect("Failed to get parent path of  current exe")
-        .join("resources").join("profiler").join("start_script.py").to_path_buf();
+        .join("resources")
+        .join("profiler")
+        .join("start_script.py")
+        .to_path_buf();
     let Some(path) = profiler_path.to_str() else {
         return;
     };
@@ -447,10 +402,9 @@ fn main() {
                 eprintln!("{}", formatted);
             }
             exit(output.status.code().unwrap_or(0))
-        },
+        }
         Err(e) => {
             eprintln!("Failed to start server.Error:{e:?}")
-        },
+        }
     };
-
 }
