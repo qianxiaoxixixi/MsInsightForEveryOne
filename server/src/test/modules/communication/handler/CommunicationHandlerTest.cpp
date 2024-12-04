a19cc5884baaf73891d5cd7c9701781b2b290ab5
insightDeveloper <1390732074@qq.com>
2024-12-04 19:14:18 +0800

    通信模块测试用例补充
    # message auto-generated for no-merge-commit merge:
    merge ******_dt into master
    
    通信模块handler、pocotol测试用例补充
    
    Created-by: insightDeveloper
    
    Match-id-0b9e32aaf38740c4b6d2369d52463879af3c19cd

---
+++ server/src/test/modules/communication/handler/CommunicationHandlerTest.cpp
@@ -0,0 +1,217 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
+ */
+
+#include <gtest/gtest.h>
+#include "CommunicationProtocolRequest.h"
+#include "../../timeline/handler/HandlerTest.cpp"
+#include "CommunicationOperatorListsHandler.h"
+#include "BandwidthHandler.h"
+#include "CommunicationOperatorDetailsHandler.h"
+#include "CommunicatorGroupQueryHandler.h"
+#include "DistributionHandler.h"
+#include "GroupHandler.h"
+#include "IterationsHandler.h"
+#include "MatrixListHandler.h"
+#include "MatrixSortOpNamesHandler.h"
+#include "OperatorNamesHandler.h"
+#include "RanksHandler.h"
+
+
+using namespace Dic::Module;
+using namespace Dic::Module::Communication;
+
+TEST_F(HandlerTest, QueryCommunicationOperatorListsWithParamError)
+{
+    auto request = std::make_unique<DurationListRequest>();
+    request->params.iterationId = ";";
+    request->params.operatorName = "opName";
+    request->params.stage = "1";
+    CommunicationOperatorListsHandler handler;
+    bool result = handler.HandleRequest(std::move(request));
+    EXPECT_EQ(result, false);
+}
+
+
+TEST_F(HandlerTest, QueryCommunicationOperatorListsWithExeSqlFail)
+{
+    auto request = std::make_unique<DurationListRequest>();
+    request->params.iterationId = "1";
+    request->params.operatorName = "opName";
+    request->params.stage = "1";
+    CommunicationOperatorListsHandler handler;
+    bool result = handler.HandleRequest(std::move(request));
+    EXPECT_EQ(result, false);
+}
+
+TEST_F(HandlerTest, QueryBandwidthHandlerParamError)
+{
+    auto request = std::make_unique<BandwidthDataRequest>();
+    request->params.iterationId = ";";
+    request->params.operatorName = "opName";
+    request->params.stage = "1";
+    BandwidthHandler handler;
+    bool result = handler.HandleRequest(std::move(request));
+    EXPECT_EQ(result, false);
+}
+
+TEST_F(HandlerTest, QueryBandwidthHandlerWithExeSqlFail)
+{
+    auto request = std::make_unique<BandwidthDataRequest>();
+    request->params.iterationId = "1";
+    request->params.operatorName = "opName";
+    request->params.stage = "1";
+    BandwidthHandler handler;
+    bool result = handler.HandleRequest(std::move(request));
+    EXPECT_EQ(result, false);
+}
+
+TEST_F(HandlerTest, CommunicationOperatorDetailsParamError)
+{
+    auto request = std::make_unique<OperatorDetailsRequest>();
+    request->params.iterationId = ";";
+    request->params.orderBy = "orderBy";
+    request->params.order = "order";
+    request->params.stage = "1";
+    CommunicationOperatorDetailsHandler handler;
+    bool result = handler.HandleRequest(std::move(request));
+    EXPECT_EQ(result, false);
+}
+
+TEST_F(HandlerTest, CommunicationOperatorDetailsWithExeSqlFail)
+{
+    auto request = std::make_unique<OperatorDetailsRequest>();
+    request->params.iterationId = "1";
+    request->params.orderBy = "orderBy";
+    request->params.order = "order";
+    request->params.stage = "1";
+    CommunicationOperatorDetailsHandler handler;
+    bool result = handler.HandleRequest(std::move(request));
+    EXPECT_EQ(result, false);
+}
+
+TEST_F(HandlerTest, CommunicatorGroupQueryHandlerWithExeSqlFail)
+{
+    auto request = std::make_unique<CommunicatorGroupRequest>();
+    CommunicatorGroupQueryHandler handler;
+    bool result = handler.HandleRequest(std::move(request));
+    EXPECT_EQ(result, false);
+}
+
+TEST_F(HandlerTest, DistributionHandlerParamError)
+{
+    auto request = std::make_unique<DistributionDataRequest>();
+    request->params.iterationId = ";";
+    request->params.operatorName = "name";
+    request->params.transportType = "type";
+    request->params.stage = "1";
+    DistributionHandler handler;
+    bool result = handler.HandleRequest(std::move(request));
+    EXPECT_EQ(result, false);
+}
+
+TEST_F(HandlerTest, DistributionHandlerWithExeSqlFail)
+{
+    auto request = std::make_unique<DistributionDataRequest>();
+    request->params.iterationId = "1";
+    request->params.operatorName = "name";
+    request->params.transportType = "type";
+    request->params.stage = "1";
+    DistributionHandler handler;
+    bool result = handler.HandleRequest(std::move(request));
+    EXPECT_EQ(result, false);
+}
+
+TEST_F(HandlerTest, GroupHandlerParamError)
+{
+    auto request = std::make_unique<MatrixGroupRequest>();
+    request->params.iterationId = ";";
+    GroupHandler handler;
+    bool result = handler.HandleRequest(std::move(request));
+    EXPECT_EQ(result, false);
+}
+
+TEST_F(HandlerTest, GroupHandlerWithExeSqlFail)
+{
+    auto request = std::make_unique<MatrixGroupRequest>();
+    request->params.iterationId = "1";
+    GroupHandler handler;
+    bool result = handler.HandleRequest(std::move(request));
+    EXPECT_EQ(result, false);
+}
+
+TEST_F(HandlerTest, IterationsHandlerWithExeSqlFail)
+{
+    auto request = std::make_unique<IterationsRequest>();
+    IterationsHandler handler;
+    bool result = handler.HandleRequest(std::move(request));
+    EXPECT_EQ(result, false);
+}
+
+TEST_F(HandlerTest, MatrixListHandlerParamError)
+{
+    auto request = std::make_unique<MatrixBandwidthRequest>();
+    request->params.iterationId = ";";
+    request->params.operatorName = "name";
+    request->params.stage = "1";
+    MatrixListHandler handler;
+    bool result = handler.HandleRequest(std::move(request));
+    EXPECT_EQ(result, false);
+}
+
+TEST_F(HandlerTest, MatrixListHandlerWithExeSqlFail)
+{
+    auto request = std::make_unique<MatrixBandwidthRequest>();
+    request->params.iterationId = "1";
+    request->params.operatorName = "name";
+    request->params.stage = "1";
+    MatrixListHandler handler;
+    bool result = handler.HandleRequest(std::move(request));
+    EXPECT_EQ(result, false);
+}
+
+TEST_F(HandlerTest, MatrixSortOpNamesHandlerWithExeSqlFail)
+{
+    auto request = std::make_unique<MatrixSortOpNamesRequest>();
+    MatrixSortOpNamesHandler handler;
+    bool result = handler.HandleRequest(std::move(request));
+    EXPECT_EQ(result, false);
+}
+
+TEST_F(HandlerTest, OperatorNamesHandlerParamError)
+{
+    auto request = std::make_unique<OperatorNamesRequest>();
+    request->params.iterationId = ";";
+    request->params.stage = "1";
+    OperatorNamesHandler handler;
+    bool result = handler.HandleRequest(std::move(request));
+    EXPECT_EQ(result, false);
+}
+
+TEST_F(HandlerTest, OperatorNamesHandlerWithExeSqlFail)
+{
+    auto request = std::make_unique<OperatorNamesRequest>();
+    request->params.iterationId = "1";
+    request->params.stage = "1";
+    OperatorNamesHandler handler;
+    bool result = handler.HandleRequest(std::move(request));
+    EXPECT_EQ(result, false);
+}
+
+TEST_F(HandlerTest, RanksHandlerParamError)
+{
+    auto request = std::make_unique<RanksRequest>();
+    request->params.iterationId = ";";
+    RanksHandler handler;
+    bool result = handler.HandleRequest(std::move(request));
+    EXPECT_EQ(result, false);
+}
+
+TEST_F(HandlerTest, RanksHandlerWithExeSqlFail)
+{
+    auto request = std::make_unique<RanksRequest>();
+    request->params.iterationId = "1";
+    RanksHandler handler;
+    bool result = handler.HandleRequest(std::move(request));
+    EXPECT_EQ(result, false);
+}
---
+++ server/src/test/modules/communication/protocol/CommunicationProtocolUtilTest.cpp
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
+ */
+
+#include <gtest/gtest.h>
+#include <GlobalDefs.h>
+#include "ProtocolDefs.h"
+#include "CommunicationProtocol.h"
+#include "CommunicationProtocolResponse.h"
+
+using namespace Dic::Protocol;
+const uint64_t NUMBER_HUNDRED = 100;
+const uint64_t NUMBER_THOUSAND = 100;
+class CommunicationProtocolUtilTest : public ::testing::Test {
+protected:
+    void SetUp() override
+    {
+        protocol.Register();
+    }
+
+    void TearDown() override
+    {
+        protocol.UnRegister();
+    }
+
+    Dic::Protocol::CommunicationProtocol protocol;
+};
+
+TEST_F(CommunicationProtocolUtilTest, ToOperatorListResponseTest)
+{
+    const std::string KEY_MIN_TIME = "minTime";
+    const std::string KEY_MAX_TIME = "maxTime";
+    Dic::Protocol::OperatorListsResponse response;
+    std::string err;
+    OperatorListsResponseBody body;
+    response.body.rankLists.emplace_back("1");
+    response.body.minTime = NUMBER_HUNDRED;
+    response.body.maxTime = NUMBER_THOUSAND;
+    OperatorTimeItem op = {"op1", 100, 900};
+    response.body.opLists.push_back(std::vector<OperatorTimeItem>{op});
+    std::optional<Dic::document_t> jsonOptional = protocol.ToJson(response, err);
+    EXPECT_EQ(jsonOptional.value()["body"][KEY_MIN_TIME.c_str()], response.body.minTime);
+    EXPECT_EQ(jsonOptional.value()["body"][KEY_MAX_TIME.c_str()], response.body.maxTime);
+}
