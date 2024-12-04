a9e14ba2c917d55a6270e3055cc2824aa6b78bea
insightDeveloper <insightDeveloper>
2024-12-04 19:14:18 +0800

    通信模块测试用例补充
    # message auto-generated for no-merge-commit merge:
    merge ******_dt into master
    
    通信模块handler、pocotol测试用例补充
    
    Created-by: insightDeveloper
    
    Match-id-0b9e32aaf38740c4b6d2369d52463879af3c19cd

---
+++ server/src/test/modules/communication/protocol/CommunicationProtocolRequestTest.cpp
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
+ */
+
+#include <gtest/gtest.h>
+#include "CommunicationProtocolRequest.h"
+
+const int NUMBER_TEN = 10;
+const int NUMBER_ONE = 1;
+class CommunicationProtocolRequestTest : public ::testing::Test {};
+
+TEST_F(CommunicationProtocolRequestTest, OperatorDetailsParamTest)
+{
+    Dic::Protocol::OperatorDetailsParam base;
+    base.iterationId = "1";
+    base.orderBy = "orderBy";
+    base.order = "order";
+    base.stage = "stage";
+    base.pageSize = NUMBER_ONE;
+    base.currentPage = NUMBER_TEN;
+    Dic::Protocol::OperatorDetailsParam param1(base);
+    param1.iterationId = ";";
+    Dic::Protocol::OperatorDetailsParam param3(base);
+    param3.orderBy = ";";
+    Dic::Protocol::OperatorDetailsParam param4(base);
+    param4.order = ";";
+    Dic::Protocol::OperatorDetailsParam param5(base);
+    param5.stage = ";";
+    std::string msg;
+    EXPECT_EQ(param1.CheckParams(msg), false);
+    EXPECT_EQ(param3.CheckParams(msg), false);
+    EXPECT_EQ(param4.CheckParams(msg), false);
+    EXPECT_EQ(param5.CheckParams(msg), false);
+}
+
+TEST_F(CommunicationProtocolRequestTest, BandwidthDataParamTest)
+{
+    Dic::Protocol::BandwidthDataParam param1 = {"1", ";", "1", "opName", "stage"};
+    Dic::Protocol::BandwidthDataParam param2 = {"1", "1", "1", ";", "stage"};
+    Dic::Protocol::BandwidthDataParam param3 = {"1", "1", "1", "opName", ";"};
+    std::string msg;
+    EXPECT_EQ(param1.CheckParams(msg), false);
+    EXPECT_EQ(param2.CheckParams(msg), false);
+    EXPECT_EQ(param3.CheckParams(msg), false);
+}
+
+TEST_F(CommunicationProtocolRequestTest, DistributionDataParamTest)
+{
+    Dic::Protocol::DistributionDataParam param1 = {"1", ";", "1", "opName", "type", "stage"};
+    Dic::Protocol::DistributionDataParam param2 = {"1", "1", "1", ";", "type", "stage"};
+    Dic::Protocol::DistributionDataParam param3 = {"1", "1", "1", "opName", ";", "stage"};
+    Dic::Protocol::DistributionDataParam param4 = {"1", "1", "1", "opName", "type", ";"};
+    std::string msg;
+    EXPECT_EQ(param1.CheckParams(msg), false);
+    EXPECT_EQ(param2.CheckParams(msg), false);
+    EXPECT_EQ(param3.CheckParams(msg), false);
+    EXPECT_EQ(param4.CheckParams(msg), false);
+}
+
+TEST_F(CommunicationProtocolRequestTest, RanksParamsTest)
+{
+    Dic::Protocol::RanksParams param1 = {"1", ";"};
+    std::string msg;
+    EXPECT_EQ(param1.CheckParams(msg), false);
+}
+
+TEST_F(CommunicationProtocolRequestTest, OperatorNamesParamsTest)
+{
+    Dic::Protocol::OperatorNamesParams base;
+    base.dbIndex = "1";
+    base.iterationId = "1";
+    base.rankList.emplace_back("1");
+    base.stage = "stage";
+    Dic::Protocol::OperatorNamesParams param1(base);
+    param1.iterationId = ";";
+    Dic::Protocol::OperatorNamesParams param2(base);
+    param2.stage = ";";
+    std::string msg;
+    EXPECT_EQ(param1.CheckParams(msg), false);
+    EXPECT_EQ(param2.CheckParams(msg), false);
+}
+
+TEST_F(CommunicationProtocolRequestTest, DurationListParamsTest)
+{
+    Dic::Protocol::DurationListParams base;
+    base.dbIndex = "1";
+    base.iterationId = "1";
+    base.operatorName = "opName";
+    base.stage = "stage";
+    Dic::Protocol::DurationListParams param1(base);
+    param1.iterationId = ";";
+    Dic::Protocol::DurationListParams param2(base);
+    param2.operatorName = ";";
+    Dic::Protocol::DurationListParams param3(base);
+    param3.stage = ";";
+    std::string msg;
+    EXPECT_EQ(param1.CheckParams(msg), false);
+    EXPECT_EQ(param2.CheckParams(msg), false);
+    EXPECT_EQ(param3.CheckParams(msg), false);
+}
+
+TEST_F(CommunicationProtocolRequestTest, MatrixGroupParamTest)
+{
+    Dic::Protocol::MatrixGroupParam param1 = {";"};
+    std::string msg;
+    EXPECT_EQ(param1.CheckParams(msg), false);
+}
+
+TEST_F(CommunicationProtocolRequestTest, MatrixBandwidthParamTest)
+{
+    Dic::Protocol::MatrixBandwidthParam param1 = {"stage", "opName", ";"};
+    Dic::Protocol::MatrixBandwidthParam param2 = {"stage", ";", "1"};
+    Dic::Protocol::MatrixBandwidthParam param3 = {";", "opName", "1"};
+    std::string msg;
+    EXPECT_EQ(param1.CheckParams(msg), false);
+    EXPECT_EQ(param2.CheckParams(msg), false);
+    EXPECT_EQ(param3.CheckParams(msg), false);
+}
