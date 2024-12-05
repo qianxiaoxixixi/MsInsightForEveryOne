e869f0acbc69d9f4e8d2915da22799a8f1861b67
insightDeveloper <1390732074@qq.com>
2024-12-05 17:42:32 +0800

    operator 部分dt补充
    # message auto-generated for no-merge-commit merge:
    merge addut1205 into master
    
    operator 部分dt补充
    
    Created-by: insightDeveloper
    
    Match-id-a000f7969449d17ab65acdd03bc880e664d03c68

--- server/src/test/modules/operator/handler/OperatorRequestHandlerTest.cpp
+++ server/src/test/modules/operator/handler/OperatorRequestHandlerTest.cpp
@@ -4,15 +4,25 @@
 
 #include <gtest/gtest.h>
 #include <WsSessionManager.h>
+#include "BaselineManagerService.h"
+#include "DataBaseManager.h"
+#include "DbSummaryDataBase.h"
+#include "FileUtil.h"
 #include "OperatorRequestHandler.h"
 #include "QueryOpCategoryInfoHandler.h"
 #include "QueryOpComputeUnitHandler.h"
 #include "QueryOpStatisticInfoHandler.h"
 #include "QueryOpDetailInfoHandler.h"
 #include "QueryOpMoreInfoHandler.h"
+#include "ParamsParser.h"
+#include "ProjectExplorerManager.h"
 #include "WsSessionImpl.h"
+#include "../../../FullDbTestSuit.cpp"
 
 using namespace Dic::Server;
+using namespace Dic::Module::Timeline;
+using namespace Dic::Module::FullDb;
+using namespace Dic::Module::Global;
 class OperatorRequestHandlerTest : public ::testing::Test {
 public:
     static void SetUpTestSuite()
@@ -29,6 +39,96 @@ public:
             session->WaitForExit();
             Dic::Server::WsSessionManager::Instance().RemoveSession();
         }
+    }
+
+    static void InitDbManager()
+    {
+        std::string currPath = Dic::FileUtil::GetCurrPath();
+        const ParamsOption &option = ParamsParser::Instance().GetOption();
+        ServerLog::Initialize(option.logPath, option.logSize, option.logLevel, to_string(option.wsPort));
+        int index = currPath.find_last_of("server");
+        currPath = currPath.substr(0, index + 1);
+        std::string dbPath3 = R"(/src/test/test_data/full_db/)";
+        DataBaseManager::Instance().SetDataType(DataType::DB);
+        auto summeryDatabase =
+            std::dynamic_pointer_cast<DbSummaryDataBase, Dic::Module::Summary::VirtualSummaryDataBase>(
+                DataBaseManager::Instance().GetSummaryDatabase("2"));
+        summeryDatabase->OpenDb(currPath + dbPath3 + "msprof_0.db", false);
+    }
+
+    static void InitBaseLineManager()
+    {
+        std::string systemDbPath = currPath.substr(0, index + 1) + R"(/src/test/test_data/)";
+        ProjectExplorerManager::Instance().InitSystemMemoryDbPath(systemDbPath);
+        InitProjectExplorerData();
+    }
+
+    static bool SetBaseLineManager()
+    {
+        InitBaseLineManager();
+        // 创建DB场景的baseline基线manager
+        std::string filePathText = currPath.substr(0, index + 1) +
+                                   R"(/src/test/test_data/test_rank_0/ASCEND_PROFILER_OUTPUT)";
+        BaselineInfo baselineInfo;
+        bool result = BaselineManagerService::InitBaselineData("testProject", filePathText, baselineInfo);
+        std::string notFinishTask = "";
+        int index = 0;
+        while (index < retry && !Dic::Module::Timeline::ParserStatusManager::Instance().IsAllFinished(notFinishTask)) {
+            const int sleepTime = 2000;
+            std::this_thread::sleep_for(std::chrono::milliseconds(sleepTime));
+            index++;
+        }
+        return result;
+    }
+
+    static void ClearProjectExplorerData()
+    {
+        ProjectExplorerManager::Instance().DeleteProjectAndFilePath("testProject",
+                                                                    std::vector<std::string>());
+        ProjectExplorerManager::Instance().DeleteProjectAndFilePath("testProjectDb",
+                                                                    std::vector<std::string>());
+    }
+
+protected:
+    inline static std::string currPath = Dic::FileUtil::GetCurrPath();
+    inline static int index = currPath.find_last_of("server");
+    inline static int retry = 5;
+    static ProjectExplorerInfo CreateProjectData(const std::string &projectName, const std::string &fileName,
+                                                 const std::string &importType, Dic::ProjectTypeEnum projectType,
+                                                 const std::vector<std::string> parseFileList)
+    {
+        ProjectExplorerInfo info;
+        info.projectName = projectName;
+        info.fileName = fileName;
+        info.importType = importType;
+        info.projectType = static_cast<int64_t>(projectType);
+        for (const auto &item: parseFileList) {
+            ParseFileInfo parseFileInfo;
+            parseFileInfo.parseFilePath = item;
+            info.parseFilePathInfos.push_back(parseFileInfo);
+        }
+        return info;
+    }
+
+    static void InitProjectExplorerData()
+    {
+        std::string filePathText = currPath.substr(0, index + 1) +
+                                   R"(/src/test/test_data/test_rank_0/ASCEND_PROFILER_OUTPUT)";
+        std::string filePathDb = currPath.substr(0, index + 1) +
+                                 R"(/src/test/test_data/full_db/ascend_pytorch_profiler.db)";
+        std::vector<ProjectExplorerInfo> infos;
+        std::vector<std::string> parseFileList {filePathText};
+        ProjectExplorerInfo info = CreateProjectData("testProject", "projectFilePath",
+                                                     "import", Dic::ProjectTypeEnum::TEXT_CLUSTER, parseFileList);
+        infos.push_back(info);
+        ProjectExplorerManager::Instance().SaveProjectExplorer(infos, false);
+
+        std::vector<ProjectExplorerInfo> dbInfos;
+        std::vector<std::string> parseDbFileList {filePathDb};
+        ProjectExplorerInfo dbInfo = CreateProjectData("testProjectDb", "projectFilePathDb",
+                                                       "import", Dic::ProjectTypeEnum::DB, parseDbFileList);
+        dbInfos.push_back(dbInfo);
+        ProjectExplorerManager::Instance().SaveProjectExplorer(dbInfos, false);
     }
 };
 
@@ -83,4 +183,102 @@ TEST_F(OperatorRequestHandlerTest, QueryOpMoreInfoHandlerNormalTest)
     Dic::Module::Operator::QueryOpMoreInfoHandler handler;
     auto requestPtr = std::make_unique<Dic::Protocol::OperatorMoreInfoRequest>();
     ASSERT_NO_THROW(handler.HandleRequest(std::move(requestPtr)));
+}
+
+TEST_F(OperatorRequestHandlerTest, QueryOpStatisticInfoHandlerSuccessWhenBaselineIsDbGroupByOperatorType)
+{
+    InitDbManager();
+    Dic::Module::Operator::QueryOpStatisticInfoHandler handler;
+    auto requestPtr = std::make_unique<Dic::Protocol::OperatorStatisticInfoRequest>();
+    requestPtr.get()->params.rankId = "2";
+    requestPtr.get()->params.group = "Operator Type";
+    requestPtr.get()->params.topK = -1; // -1 表示topK取全部数据
+    requestPtr.get()->params.isCompare = true;
+    // 10 表示分页最小是10条
+    requestPtr.get()->params.pageSize = 10;
+    requestPtr.get()->params.current = 1;
+
+    EXPECT_TRUE(SetBaseLineManager());
+    EXPECT_TRUE(handler.HandleRequest(std::move(requestPtr)));
+    ClearProjectExplorerData();
+}
+
+TEST_F(OperatorRequestHandlerTest, QueryOpStatisticInfoHandlerSuccessAndOrderByOpTypeAndTopKIs15GroupByInputShape)
+{
+    InitDbManager();
+    Dic::Module::Operator::QueryOpStatisticInfoHandler handler;
+    auto requestPtr = std::make_unique<Dic::Protocol::OperatorStatisticInfoRequest>();
+    requestPtr.get()->params.rankId = "2";
+    requestPtr.get()->params.group = "Input Shape";
+    requestPtr.get()->params.topK = 15;  // 15表示topK取15条数据
+    requestPtr.get()->params.isCompare = true;
+    requestPtr.get()->params.orderBy = "op_type";
+    // 10 表示分页最小是10条
+    requestPtr.get()->params.pageSize = 10;
+    requestPtr.get()->params.current = 1;
+
+    EXPECT_TRUE(SetBaseLineManager());
+    EXPECT_TRUE(handler.HandleRequest(std::move(requestPtr)));
+    ClearProjectExplorerData();
+}
+
+TEST_F(OperatorRequestHandlerTest, QueryOpStatisticInfoHandlerSuccessWhenBaselineIsDbGroupByHCCLOperatorType)
+{
+    InitDbManager();
+    Dic::Module::Operator::QueryOpStatisticInfoHandler handler;
+    auto requestPtr = std::make_unique<Dic::Protocol::OperatorStatisticInfoRequest>();
+    requestPtr.get()->params.rankId = "2";
+    requestPtr.get()->params.group = "HCCL Operator Type";
+    // topK给一个极大值
+    requestPtr.get()->params.topK = 10000000; // 10000000表示topK是一个极大值
+    requestPtr.get()->params.isCompare = true;
+    requestPtr.get()->params.orderBy = "count";
+    requestPtr.get()->params.order = "ascend";
+    // 10 表示分页最小是10条
+    requestPtr.get()->params.pageSize = 10;
+    requestPtr.get()->params.current = 1;
+
+    EXPECT_TRUE(SetBaseLineManager());
+    EXPECT_TRUE(handler.HandleRequest(std::move(requestPtr)));
+    ClearProjectExplorerData();
+}
+
+TEST_F(OperatorRequestHandlerTest, QueryOpStatisticInfoHandlerSuccessGroupByHCCLOperatorTypeOrderDesc)
+{
+    InitDbManager();
+    Dic::Module::Operator::QueryOpStatisticInfoHandler handler;
+    auto requestPtr = std::make_unique<Dic::Protocol::OperatorStatisticInfoRequest>();
+    requestPtr.get()->params.rankId = "2";
+    requestPtr.get()->params.group = "HCCL Operator Type";
+    // topK给一个极大值
+    requestPtr.get()->params.topK = 10000000; // 10000000表示topK是一个极大值
+    requestPtr.get()->params.isCompare = true;
+    requestPtr.get()->params.orderBy = "count";
+    requestPtr.get()->params.order = "descend";
+    // 10 表示分页最小是10条
+    requestPtr.get()->params.pageSize = 10;
+    requestPtr.get()->params.current = 1;
+
+    EXPECT_TRUE(SetBaseLineManager());
+    EXPECT_TRUE(handler.HandleRequest(std::move(requestPtr)));
+    ClearProjectExplorerData();
+}
+
+// QueryOpDetailInfoHandler 测试
+TEST_F(OperatorRequestHandlerTest, QueryOpDetailInfoHandlerFailedWhenBsesLineIsNotSet)
+{
+    Dic::Module::Operator::QueryOpDetailInfoHandler handler;
+    auto requestPtr = std::make_unique<Dic::Protocol::OperatorDetailInfoRequest>();
+    requestPtr.get()->params.rankId = "2";
+    requestPtr.get()->params.group = "Operator";
+    // topK给一个极大值
+    requestPtr.get()->params.topK = 10000000; // 10000000表示topK是一个极大值
+    requestPtr.get()->params.isCompare = true;
+    requestPtr.get()->params.orderBy = "count";
+    requestPtr.get()->params.order = "descend";
+    // 10 表示分页最小是10条
+    requestPtr.get()->params.pageSize = 10;
+    requestPtr.get()->params.current = 1;
+
+    EXPECT_FALSE(handler.HandleRequest(std::move(requestPtr)));
 }
