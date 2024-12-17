7aae77d85474f3abbdeb9c0bebc395a55e3f553d
insightDeveloper <insightDeveloper>
2024-12-17 19:42:28 +0800

    【********】域维度层次化分析性能差异-增加handler接口
    # message auto-generated for no-merge-commit merge:
    merge master_parallelism_handler1217 into master
    
    【********】域维度层次化分析性能差异-增加handler接口（含UT）
    
    Created-by: insightDeveloper
    
    Match-id-56ffe9b80b3c5dd6a245fa5772e96eedfbd6eb09

---
+++ server/src/modules/summary/handler/QueryParallelismPerformanceHandler.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
+ */
+
+#ifndef PROFILER_SERVER_SERVER_SRC_MODULES_SUMMARY_HANDLER_QUERYPARALLELISMPERFORMANCEHANDLER_H
+#define PROFILER_SERVER_SERVER_SRC_MODULES_SUMMARY_HANDLER_QUERYPARALLELISMPERFORMANCEHANDLER_H
+
+#include "SummaryProtocolResponse.h"
+#include "SummaryRequestHandler.h"
+namespace Dic::Module::Summary {
+class QueryParallelismPerformanceHandler : public SummaryRequestHandler  {
+public:
+    QueryParallelismPerformanceHandler()
+    {
+        command = Protocol::REQ_RES_PARALLELISM_PERFORMANCE_DATA;
+    }
+    ~QueryParallelismPerformanceHandler() override = default;
+    bool HandleRequest(std::unique_ptr<Protocol::Request> requestPtr) override;
+};
+}
+#endif // PROFILER_SERVER_SERVER_SRC_MODULES_SUMMARY_HANDLER_QUERYPARALLELISMPERFORMANCEHANDLER_H
