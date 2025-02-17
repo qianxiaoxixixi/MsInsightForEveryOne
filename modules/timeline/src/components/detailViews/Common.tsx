5826ed98e3b17d62e78f7c6cddf941dc2de6ef71
insightDeveloper <insightDeveloper>
2025-02-17 19:33:12 +0800

    feat(timeline) 前端改“HCCL 汇总”为“COMMUNICATION 汇总”，接口传HCCL暂时不变
    Match-id-40b6e7585041af48e3e113730ef1e7d766135f6d

--- modules/timeline/src/components/detailViews/Common.tsx
+++ modules/timeline/src/components/detailViews/Common.tsx
@@ -170,7 +170,7 @@ export const statsSystemViewItems: string[] = [
     'Python API Summary',
     'CANN API Summary',
     'Ascend HardWare Task Summary',
-    'HCCL Summary',
+    'COMMUNICATION Summary',
     'Overlap Analysis',
     'Kernel Details',
 ];
