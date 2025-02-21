b4685cd5e456303fd0c854495c6e8faaeb51bad5
insightDeveloper <insightDeveloper>
2025-02-21 21:30:16 +0800

    []fix(plugin) 修复Mac场景下启动APP闪退问题(过早使用LogUtil)
    # message auto-generated for no-merge-commit merge:
    merge 202502/FixMacAppStartException into master
    
    Mac场景签名后启动APP优化
    
    Created-by: insightDeveloper
    
    Match-id-c38317751457dca8e5d9ce21aab3e7a36b14cb47

--- server/msinsight/src/PluginsManager.cpp
+++ server/msinsight/src/PluginsManager.cpp
@@ -22,7 +22,6 @@ bool PluginsManager::RegisterPlugin(std::unique_ptr<BasePlugin> plugin)
     if (pluginsMap_.count(plugin->GetPluginName()) != 0) {
         return false;
     }
-    Server::ServerLog::Info("Load plugin:", plugin->GetPluginName());
     pluginsMap_.emplace(plugin->GetPluginName(), std::move(plugin));
     return true;
 }
@@ -42,7 +41,6 @@ void PluginsManager::LoadPlugins()
 #ifdef _WIN32
                 LoadLibraryA(file.path().string().c_str());
 #else
-                Server::ServerLog::Info("Load plugin so:", file.path().string());
                 dlopen(file.path().string().c_str(), RTLD_LAZY);
 #endif
             }
