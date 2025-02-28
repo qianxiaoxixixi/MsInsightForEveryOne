9648f540b02eb6a592040fb84fa1dfda02a47949
insightDeveloper <1390732074@qq.com>
2025-02-28 17:07:33 +0800

    合并poc分支代码到master分支【修改检视意见】
    Match-id-12dd19a49032014d58d29d89f64a1aaf792767ad

--- modules/compute/src/components/cacheKit/CacheKitChart.tsx
+++ modules/compute/src/components/cacheKit/CacheKitChart.tsx
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) Huawei Technologies Co., Ltd. 2023-2024. All rights reserved.
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
 */
 import React, { useEffect, useState } from 'react';
 import { CloseCircleOutlined } from '@ant-design/icons';
