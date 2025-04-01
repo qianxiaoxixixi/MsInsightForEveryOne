fedada3ae1ff8625977cf803381d8a1e87d2f413
insightDeveloper <1390732074@qq.com>
2025-04-01 10:15:09 +0800

    删除旧的framework
    # message auto-generated for no-merge-commit merge:
    merge remove-framework into master
    
    删除旧的 framework (part1)
    
    Created-by: insightDeveloper
    
    Match-id-915db837a62a2d534a931be4087e92ef9118ab56

--- framework/env.d.ts
+++
@@ -1,4 +0,0 @@
-/*
- * Copyright (c) Huawei Technologies Co., Ltd. 2023-2024. All rights reserved.
- */
-/// <reference types="vite/client" />
--- framework/src/connection/defs.ts
+++
@@ -1,4 +0,0 @@
-/*
- * Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
-*/
-export type ResponseInterceptor<T> = (event: MessageEvent, responseInterceptor: T) => void;
