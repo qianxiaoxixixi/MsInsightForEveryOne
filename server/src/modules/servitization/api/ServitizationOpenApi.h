3e3bdd85a02fe2fd8f49153d83058d714afcf0c7
insightDeveloper <insightDeveloper>
2025-05-21 15:34:54 +0800

    适配服务化db，增加模块对外api和数据查询repo
    # message auto-generated for no-merge-commit merge:
    merge master_add_mm_line3 into master
    
    适配服务化db，增加模块对外api和数据查询repo
    
    Created-by: insightDeveloper
    
    Match-id-fd5e6d2576c13d4e182818b01de967b3f1675a81

---
+++ server/src/modules/servitization/api/ServitizationOpenApi.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
+ */
+#ifndef PROFILER_SERVER_STATISTICSMODULEAPI_H
+#define PROFILER_SERVER_STATISTICSMODULEAPI_H
+#include "ServitizationContext.h"
+namespace Dic::Module::IE {
+struct TaskInfo {
+    std::string fileId;
+    std::string filePath;
+};
+class ServitizationOpenApi {
+public:
+    ServitizationOpenApi() = default;
+    ~ServitizationOpenApi() = default;
+    virtual bool Parse(const std::unordered_map<std::string, std::string>& inputs);
+    /**
+     * 广度优先找到所有有效的IE文件
+     * @param path
+     * @return
+     */
+    virtual std::vector<TaskInfo> ComputeTaskInfo(const std::string& path);
+    virtual void Reset();
+
+protected:
+    std::shared_ptr<ServitizationContext> context = std::make_shared<ServitizationContext>();
+
+private:
+    const std::string IEFileName = "profiler.db";
+    bool ValidIEFile(const std::string& path);
+    void ParseSingleFile(const std::string& filePath, const std::string& fileId);
+};
+}  // namespace Dic::Module::IE
+#endif  // PROFILER_SERVER_STATISTICSMODULEAPI_H
---
+++ server/src/test/modules/servitization/api/ServitizationOpenApiTest.cpp
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
+ */
+#include <gtest/gtest.h>
+#include "FileUtil.h"
+#include "ServitizationOpenApi.h"
+#include "ServitizationContext.h"
+using namespace Dic::Module::IE;
+class ServitizationOpenApiTest : public ::testing::Test {};
+
+TEST_F(ServitizationOpenApiTest, InitDataBaseTest)
+{
+    std::shared_ptr<ServitizationOpenApi> openApi = std::make_shared<ServitizationOpenApi>();
+    std::string folder = Dic::FileUtil::GetCurrPath();
+    std::string tempFileName = Dic::FileUtil::SplicePath(folder, "profiler.db");
+    std::string tempErrFileName = Dic::FileUtil::SplicePath(Dic::FileUtil::GetCurrPath(), "mmmm.text");
+    Dic::Server::ServerLog::Info(tempFileName);
+    std::ofstream tempFile(tempFileName, std::ios::out | std::ios::trunc);
+    std::ofstream tempErrFile(tempErrFileName, std::ios::out | std::ios::trunc);
+    auto res = openApi->ComputeTaskInfo(folder);
+    EXPECT_EQ(res.size(), 1);  // 1
+    openApi->Reset();
+}
+
+TEST_F(ServitizationOpenApiTest, ParseTest)
+{
+    std::shared_ptr<ServitizationOpenApi> openApi = std::make_shared<ServitizationOpenApi>();
+    std::string folder = Dic::FileUtil::GetCurrPath();
+    std::string tempFileName = Dic::FileUtil::SplicePath(Dic::FileUtil::GetCurrPath(), "profiler.db");
+    std::string tempErrFileName = Dic::FileUtil::SplicePath(Dic::FileUtil::GetCurrPath(), "mmmm.text");
+    std::ofstream tempFile(tempFileName, std::ios::out | std::ios::trunc);
+    std::ofstream tempErrFile(tempErrFileName, std::ios::out | std::ios::trunc);
+    std::unordered_map<std::string, std::string> inputs;
+    inputs["lll"] = folder;
+    auto res = openApi->Parse(inputs);
+    EXPECT_EQ(res, true);
+    openApi->Reset();
+    inputs["lll"] = tempErrFileName;
+    res = openApi->Parse(inputs);
+    EXPECT_EQ(res, false);
+    openApi->Reset();
+    inputs["lll"] = tempFileName;
+    res = openApi->Parse(inputs);
+    EXPECT_EQ(res, true);
+    openApi->Reset();
+}
