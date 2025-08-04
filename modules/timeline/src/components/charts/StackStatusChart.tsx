17bc82109e2a8dec6a9bcf3d1371cc8543710af0
insightDeveloper <insightDeveloper>
2025-08-04 10:38:49 +0800

    修复合并用后选中算子出现多个泳道中描边
    # message auto-generated for no-merge-commit merge:
    merge bugfix-selectedOpRepeated into master
    
    【********】修复合并泳道后选中算子出现多个泳道中描边
    
    Created-by: insightDeveloper
    
    Match-id-efe4000ade1a05102209b07a58dc7b7500d1a84a

--- modules/timeline/src/components/charts/StackStatusChart.tsx
+++ modules/timeline/src/components/charts/StackStatusChart.tsx
@@ -238,7 +238,7 @@ const mouseUpFunc = ({ e, datasState, rangeAndDomain, rowHeight, session, metada
     }
     runInAction(() => {
         session.selectedData = clickedData
-            ? { ...clickedData, threadId: (metadata as ThreadMetaData).threadId ?? '', processId: (metadata as ThreadMetaData).processId ?? '' }
+            ? { ...clickedData, threadId: clickedData.threadId ?? (metadata as ThreadMetaData).threadId ?? '', processId: (metadata as ThreadMetaData).processId ?? '' }
             : undefined;
         onClick?.(clickedData, session, metadata);
         session.selectedRangeData = undefined;
