854fd757d5ec1da826e7e437010d5844ba1d2243
insightDeveloper <insightDeveloper>
2025-08-25 19:50:25 +0800

    修复hccl相关算子在更多界面无索引数据
    # message auto-generated for no-merge-commit merge:
    merge fixbug_****** into master
    
    【********】【继承特性】【MindStudio ******】【Insight】Timeline界面Communication下hccl相关算子在更多界面无索引数据
    
    Created-by: insightDeveloper
    
    Match-id-7690d38229f03777d8b7dfc54224332b2c10adb6

--- server/src/modules/defs/DbSqlDefs.h
+++ server/src/modules/defs/DbSqlDefs.h
@@ -81,17 +81,17 @@ inline std::string GetHcclSameNameDetailSql(const std::string &tidListStr, const
     "   join params p where deviceId = p.rankId and groupName || '_' || planeId in (" + tidListStr +
     "   ) and timestamp+duration >= p.startTime AND timestamp <= p.endTime "
     + (uniqueDevice
-        ? " UNION ALL select op.startNs - p.minTime as timestamp, op.endNs - op.startNs as duration, 0 as depth, "
-            "   op.ROWID as id , op.groupName || 'group' as tid, 'HCCL' as pid from COMMUNICATION_OP op "
-            "   join COMMUNICATION_TASK_INFO c ON op.opId = c.opId "
-            "   join TASK main on c.globalTaskId = main.globalTaskId "
-            "   join nameIds on op.opName = id join params p "
-            "   where deviceId = p.rankId and op.groupName||'group' in (" + tidListStr +
-            "   ) and timestamp+duration >= p.startTime AND timestamp <= p.endTime group by op.opId "
-        : " UNION ALL select op.startNs - minTime as timestamp, op.endNs - op.startNs as duration, 0 as depth, "
-            "   op.ROWID as id , op.groupName || 'group' as tid, 'HCCL' as pid from COMMUNICATION_OP op "
-            "   join nameIds on op.opName = id join params p where op.groupName || 'group' in (" + tidListStr +
-            "   ) and timestamp + duration >= p.startTime AND timestamp <= p.endTime group by op.opId ")
+        ? " UNION ALL select op.startNs - minTime as timestamp, op.endNs - op.startNs as duration, 0 as depth, "
+          "   op.opId as id , op.groupName || 'group' as tid, 'HCCL' as pid from COMMUNICATION_OP op "
+          "   join nameIds on op.opName = id join params p where op.groupName || 'group' in (" + tidListStr +
+          "   ) and timestamp + duration >= p.startTime AND timestamp <= p.endTime group by op.opId "
+        : " UNION ALL select op.startNs - p.minTime as timestamp, op.endNs - op.startNs as duration, 0 as depth, "
+          "   op.opId as id , op.groupName || 'group' as tid, 'HCCL' as pid from COMMUNICATION_OP op "
+          "   join COMMUNICATION_TASK_INFO c ON op.opId = c.opId "
+          "   join TASK main on c.globalTaskId = main.globalTaskId "
+          "   join nameIds on op.opName = id join params p "
+          "   where deviceId = p.rankId and op.groupName||'group' in (" + tidListStr +
+          "   ) and timestamp+duration >= p.startTime AND timestamp <= p.endTime group by op.opId ")
     + ") ";
 }
 inline std::string GetCannSameNameDetailSql(const std::string &pidListStr, const std::string &tidListStr)
