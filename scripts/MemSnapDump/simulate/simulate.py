ffb1e8187d8e09701fa0823a16ce6f7169387838
ascend-robot <zhongyuanke@huawei.com>
2026-04-15 10:31:52 +0800

    [issue #183][Bugfix][MemSnapshot] 内存快照补全兼容workspace兼容逻辑，增加健壮性
    Co-authored-by: codeasier<liuyekang@huawei.com>

--- scripts/MemSnapDump/simulate/simulate.py
+++ scripts/MemSnapDump/simulate/simulate.py
@@ -41,7 +41,7 @@ class SimulateDeviceSnapshot:
         self._replay_logger = replay_logger.getChild(f"{device}")
         if not snapshot_dict:
             raise RuntimeError("Cannot init snapshot from empty data.")
-        self._loading_logger.info(f"Loading snapshot data...")
+        self._loading_logger.info("Loading snapshot data...")
         self.device_snapshot = DeviceSnapshot.from_dict(snapshot_dict, device, ignore_inactive_blocks=True)
         self._loading_logger.info(f"Finished to load snapshot data: total of {len(self.device_snapshot.trace_entries)} "
                                   f"entries and {len(self.device_snapshot.segments)} segments.")
@@ -51,6 +51,57 @@ class SimulateDeviceSnapshot:
         # 识别昇腾torch-npu采集的snapshot中的workspace事件
         if self.device_snapshot.trace_entries and self.device_snapshot.trace_entries[0].action == 'workspace_snapshot':
             self.simulated_allocator_context.workspace_flag = True
+            # 适配npu workspace问题
+            self._adapt_workspace_snapshot()
+
+    def _adapt_workspace_snapshot(self):
+        """
+        背景：
+            昇腾torch-npu在处理dump-snapshot时，粗暴的将每个dump时刻仍然存在的workspace（按流区分）池，分成三个连续的事件，且插入到snapshot事件列表的头
+            T1. workspace_snapshot事件：workspace标志事件，具有size
+            T2. segment_alloc事件：workspace segment扩容事件，size与workspace_snapshot中的size一致
+            T3. alloc事件：全部分配掉segment_alloc，与segment_alloc中的size一致
+            
+            而该粗暴的适配造成了以下几个问题：
+            1. workspace segment的状态错误
+                - 其dump时刻仅存在一个完整size的inactive Block。这与其T3. alloc事件严重不符
+                - active、allocated大小均为0，，导致数据不准确（社区Memory Viz可视化后同样存在该问题）
+                - 预期在dump时刻的snapshot中，地址与workspace_snapshot相同且stream相同的segment，应该具有以下特征：
+                    - active_size == allocated_size == total_size
+                    - blocks的数组长度为1 且 blocks[0].size == total_size 且 blocks[0].state == active_allocated
+            2. 无法真实反应workspace内存池的变化过程，仅记录dump时刻的状态。
+            
+        该方法旨在解决问题1的数据一致性问题，对于问题2需要pytorch-npu侧适配，以记录真实的内存池变化过程。
+        """
+        # 以连续事件组的形式，提取workspace相关事件
+        # 遍历到不满足上述T1->T2->T3顺序的事件组或大小不正确时结束
+        self._loading_logger.info("Recognized workspace events in snapshot, start adapting...")
+        total_events = self.device_snapshot.trace_entries
+        workspace_events_grp = []
+        cur_grp_idx_start = 0
+        while cur_grp_idx_start + 2 < len(total_events):
+            workspace_events_grp = total_events[cur_grp_idx_start:cur_grp_idx_start + 3]
+            if [event.action for event in workspace_events_grp] != ['workspace_snapshot', 'segment_alloc', 'alloc']:
+                break
+            workspace_snapshot = workspace_events_grp[0]
+            segment_idx = self.device_snapshot.find_segment_idx_by_addr(workspace_snapshot.addr, workspace_snapshot.stream)
+            if segment_idx < 0 or segment_idx >= len(self.device_snapshot.segments):
+                self._loading_logger.warning(f"Workspace snapshot at addr {workspace_snapshot.addr} (stream {workspace_snapshot.stream}) not found in device snapshot")
+                break
+            existed_seg = self.device_snapshot.segments[segment_idx]
+            existed_seg.allocated_size = workspace_snapshot.size
+            existed_seg.active_size = workspace_snapshot.size
+            existed_seg.blocks = [
+                Block(
+                    size=existed_seg.total_size,
+                    state=BlockState.ACTIVE_ALLOCATED,
+                    address=existed_seg.address,
+                    frames=existed_seg.frames,
+                    segment_ptr=existed_seg
+                )
+            ]
+            cur_grp_idx_start += 3
+        self._loading_logger.info("Finished to adapt workspace snapshot.")
 
     def register_hooker(self, hooker: SimulateHooker) -> int:
         idx = hash(hooker)
